596e13c1e15b35ff14b5001b7ab34d5f5ce66496
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-21 10:37:17 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -1,65 +1,43 @@
-import os
-from typing import OrderedDict
+import os, torch, random
 import numpy as np
-import PIL
-from PIL import Image
+from typing import OrderedDict
 from torch.utils.data import Dataset
-from torchvision import transforms
-from captionizer import caption_from_path, generic_captions_from_path
-from captionizer import find_images
+from torchvision.transforms import v2
+from torchvision.transforms.v2 import functional as fun
+from torchvision.io import decode_image
+from captionizer import caption_from_path, generic_captions_from_path, find_images
+
 
 per_img_token_list = [
     'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',
 ]
 
 class PersonalizedBase(Dataset):
-    def __init__(self,
-                 data_root,
-                 size=None,
-                 repeats=100,
-                 interpolation="bicubic",
-                 flip_p=0.5,
-                 set="train",
-                 placeholder_token="dog",
-                 per_image_tokens=False,
-                 center_crop=False,
-                 mixing_prob=0.25,
-                 coarse_class_text=None,
-                 token_only=False,
-                 reg=False
-                 ):
-
+    def __init__(self, set, reg, data_root, size, repeats, flip_p, placeholder_token, coarse_class_text,
+                token_only, per_image_tokens, center_crop, mixing_prob):
+        
+        super().__init__()
         self.data_root = data_root
-
         self.image_paths = find_images(self.data_root)
-
-        # self._length = len(self.image_paths)
         self.num_images = len(self.image_paths)
         self._length = self.num_images
-
         self.placeholder_token = placeholder_token
         self.token_only = token_only
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
         self.mixing_prob = mixing_prob
-
+        self.flip_p = flip_p
         self.coarse_class_text = coarse_class_text
-
+        self.size = size
+        self.reg = reg
+        self.repeats = repeats
+                       
         if per_image_tokens:
-            assert self.num_images < len(
-                per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
+            assert self.num_images < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
         if set == "train":
-            self._length = self.num_images * repeats
-
-        self.size = size
-        self.interpolation = {"linear": PIL.Image.LINEAR,
-                              "bilinear": PIL.Image.BILINEAR,
-                              "bicubic": PIL.Image.BICUBIC,
-                              "lanczos": PIL.Image.LANCZOS,
-                              }[interpolation]
-        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
-        self.reg = reg
+            self._length = self.num_images * self.repeats
+        
         if self.reg and self.coarse_class_text:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
 
@@ -69,32 +47,23 @@ class PersonalizedBase(Dataset):
     def __getitem__(self, i):
         example = {}
         image_path = self.image_paths[i % self.num_images]
-        image = Image.open(image_path)
-
-        if not image.mode == "RGB":
-            image = image.convert("RGB")
-
-        example["caption"] = ""
+        img = decode_image(image_path, mode="RGB")
+        transform = v2.Compose([
+            v2.ToDtype(dtype=torch.uint8, scale=True),
+            v2.RandomCrop(min(img.shape[1], img.shape[2])),
+            v2.Resize((self.size, self.size), interpolation=3, antialias=True),
+            v2.RandomAdjustSharpness(sharpness_factor=random.uniform(1.1, 1.5), p=0.5),
+            v2.GaussianBlur(kernel_size=1, sigma=(0.1, 0.5)),
+            v2.RandomHorizontalFlip(p=self.flip_p),
+            v2.RandomPerspective(distortion_scale=0.25, p=0.5, interpolation=2),
+            v2.Lambda(lambda x: x.detach().clone().permute(1, 2, 0).cpu().numpy().astype(np.uint8))
+        ])
+        example['image'] = np.array(transform(img) / 127.5 - 1.0).astype(np.float32)
+        
         if self.reg and self.coarse_class_text:
             example["caption"] = generic_captions_from_path(image_path, self.data_root, self.reg_tokens)
         else:
             example["caption"] = caption_from_path(image_path, self.data_root, self.coarse_class_text, self.placeholder_token)
-
-        # default to score-sde preprocessing
-        img = np.array(image).astype(np.uint8)
-
-        if self.center_crop:
-            crop = min(img.shape[0], img.shape[1])
-            h, w, = img.shape[0], img.shape[1]
-            img = img[(h - crop) // 2:(h + crop) // 2,
-                      (w - crop) // 2:(w + crop) // 2]
-
-        image = Image.fromarray(img)
-        if self.size is not None:
-            image = image.resize((self.size, self.size),
-                                 resample=self.interpolation)
-
-        image = self.flip(image)
-        image = np.array(image).astype(np.uint8)
-        example["image"] = (image / 127.5 - 1.0).astype(np.float32)
+        
         return example
+
