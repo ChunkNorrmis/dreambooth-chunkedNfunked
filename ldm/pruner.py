6239bbd3fef39722b2a9d730376fa00ccf34cadf
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-28 16:54:37 -0400

    Add float32 parameter to prune_checkpoint function

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,13 +1,10 @@
-from dreambooth_helpers.joepenna_dreambooth_config import JoePennaDreamboothConfigSchemaV1
-
-def prune_checkpoint(old_state):
-    precision = JoePennaDreamboothConfigSchemaV1()
+def prune_checkpoint(old_state, float32=False):
     if int(old_state['global_step']) > 0:
         print(f"This is global step {old_state['global_step']}.")
         print(f"Checkpoint Keys: {old_state.keys()}")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in old_state.items() if k != "optimizer_states" and k != 'state_dict'}
-        if precision.float32:
+        if float32:
             pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in old_state['state_dict'].items()}
             pruned_checkpoint['precision'] = 'float32'
         else:
