4d654be7e11f24e50fc94ac9246af6e92c3c3c68
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-28 17:05:24 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,10 +1,13 @@
-def prune_checkpoint(old_state, float32=False):
+from dreambooth_helpers.joepenna_dreambooth_config import JoePennaDreamboothConfigSchemaV1
+
+def prune_checkpoint(old_state):
+    conf = JoePennaDreamboothConfigSchemaV1()
     if int(old_state['global_step']) > 0:
         print(f"This is global step {old_state['global_step']}.")
         print(f"Checkpoint Keys: {old_state.keys()}")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in old_state.items() if k != "optimizer_states" and k != 'state_dict'}
-        if float32:
+        if conf.model_precision():
             pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in old_state['state_dict'].items()}
             pruned_checkpoint['precision'] = 'float32'
         else:
