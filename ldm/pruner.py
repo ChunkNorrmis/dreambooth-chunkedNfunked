bfc793c726726db98007876c4cd997f351852806
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-29 23:34:47 -0400

    Display keys of pruned checkpoint
    Added print statement to show keys of the pruned checkpoint.

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,11 +1,11 @@
 def prune_checkpoint(old_state):
     if int(old_state['global_step']) > 0:
         print(f"This is global step {old_state['global_step']}.")
-        print(f"Checkpoint Keys: {old_state.keys()}")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in old_state.items() if k != "optimizer_states" and k != 'state_dict'}
         pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in old_state['state_dict'].items()}
         pruned_checkpoint['precision'] = 'float32'
         #pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in old_state['state_dict'].items()}
         #pruned_checkpoint['precision'] = 'float16'
+        print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
