b17aa60237b113b86ff857f0e4d6cd7ea1943e4d
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-30 09:51:28 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,11 +1,14 @@
-def prune_checkpoint(old_state):
+def prune_checkpoint(old_state, fp32=False):
     if int(old_state['global_step']) > 0:
         print(f"This is global step {old_state['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in old_state.items() if k != "optimizer_states" and k != 'state_dict'}
-        pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in old_state['state_dict'].items()}
-        pruned_checkpoint['precision'] = 'float32'
-        #pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in old_state['state_dict'].items()}
-        #pruned_checkpoint['precision'] = 'float16'
+        if not fp32:
+            pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in old_state['state_dict'].items()}
+            pruned_checkpoint['precision'] = 'float16'
+        else:
+            pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in old_state['state_dict'].items()}
+            pruned_checkpoint['precision'] = 'float32'
+        
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
