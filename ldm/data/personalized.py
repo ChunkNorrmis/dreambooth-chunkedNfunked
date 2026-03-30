8ef8c9a3ef875d180a6b3f9e118f39b10ad99729
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-30 09:08:37 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -43,17 +43,27 @@ class PersonalizedBase(Dataset):
         return self._length
 
     def __getitem__(self, i):
+        def normal(image):
+            rm = float(image[[0],:,:].mean())
+            gm = float(image[[1],:,:].mean())
+            bm = float(image[[2],:,:].mean())
+            rs = float(image[[0],:,:].std())
+            gs = float(image[[1],:,:].std())
+            bs = float(image[[2],:,:].std())
+            return fun.normalize(image, mean=[rm, gm, bm], std=[rs, gs, bs])
         example = {}
         image_path = self.image_paths[i % self.num_images]
         image = decode_image(image_path, mode="RGB")
         crop = min(image.shape[1], image.shape[2])
         transform = v2.Compose([
-            v2.RandomCrop((crop, crop)) if image.shape[1] > (crop * 2) and image.shape[2] > (crop * 2) else v2.CenterCrop((crop, crop)),
+            v2.ToDtype(dtype=torch.uint8, scale=True),
+            v2.CenterCrop((crop, crop)),
             v2.Resize((self.size, self.size), interpolation=3, antialias=True),
             v2.RandomHorizontalFlip(p=self.chance),
-            v2.GaussianBlur(kernel_size=random.choice([1, 3, 5]), sigma=(0.1, 0.3)),
-            v2.Lambda(lambda x: np.array(x.detach().permute(1, 2, 0)).astype(np.uint8)),
-            v2.Lambda(lambda x: np.array(x / 127.5 - 1).astype(np.float32))
+            v2.GaussianBlur(kernel_size=1, sigma=(0.1, 0.5)),
+            v2.ToDtype(dtype=torch.float32, scale=True),
+            v2.Lambda(normal),
+            v2.Lambda(lambda x: x.permute(1, 2, 0).numpy(force=True))
         ])
         example['image'] = transform(image)
         
