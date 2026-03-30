bfa8596e3329880a9fdc2123b2411ec8ef4f106a
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-03-29 23:24:43 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -48,14 +48,12 @@ class PersonalizedBase(Dataset):
         image = decode_image(image_path, mode="RGB")
         crop = min(image.shape[1], image.shape[2])
         transform = v2.Compose([
-            v2.ToDtype(dtype=torch.uint8, scale=True),
-            (v2.RandomCrop((crop, crop)) if image.shape[1] > crop or image.shape[2] > crop else v2.CenterCrop((crop, crop))),
+            v2.RandomCrop((crop, crop)) if image.shape[1] > (crop * 2) and image.shape[2] > (crop * 2) else v2.CenterCrop((crop, crop)),
             v2.Resize((self.size, self.size), interpolation=3, antialias=True),
             v2.RandomHorizontalFlip(p=self.chance),
             v2.GaussianBlur(kernel_size=random.choice([1, 3, 5]), sigma=(0.1, 0.3)),
-            v2.ToDtype(dtype=torch.float32, scale=True),
-            v2.Normalize(mean=[torch.mean(image, 0)], std=[torch.std(image, 0)]),
-            v2.Lambda(lambda x: np.array(x.permute(1, 2, 0)).astype(np.float32))
+            v2.Lambda(lambda x: np.array(x.detach().permute(1, 2, 0)).astype(np.uint8)),
+            v2.Lambda(lambda x: np.array(x / 127.5 - 1).astype(np.float32))
         ])
         example['image'] = transform(image)
         
