f37a02d965c19d750acde2d711bbd6714e92a13a
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-01 12:43:49 -0400

    Add image to ndarray conversion and update transformations
    Added a method to convert images to ndarray format and updated the transformation pipeline to include new data types and normalization.

--- ldm/data/personalized_style.py
+++ ldm/data/personalized_style.py
@@ -80,8 +80,7 @@ class PersonalizedBase(Dataset):
         if set == "train":
             self._length = self.num_images * repeats
 
-    def __len__(self):
-        return self._length
+    def to_ndarray(self, image): return image.permute(1, 2, 0).numpy(force=True).astype(np.float32)
 
     def __getitem__(self, i):
         example = {}
@@ -89,12 +88,14 @@ class PersonalizedBase(Dataset):
         image = decode_image(image_path, mode="RGB")
         crop = min(image.shape[1], image.shape[2])
         transform = v2.Compose([
-            v2.RandomCrop((crop, crop)) if image.shape[1] > (crop * 2) and image.shape[2] > (crop * 2) else v2.CenterCrop((crop, crop)),
+            v2.ToDtype(dtype=torch.uint8, scale=True),
+            v2.CenterCrop((crop, crop)),
             v2.Resize((self.size, self.size), interpolation=3, antialias=True),
             v2.RandomHorizontalFlip(p=self.chance),
-            v2.GaussianBlur(kernel_size=random.choice([1, 3, 5]), sigma=(0.1, 0.3)),
-            v2.Lambda(lambda x: np.array(x.detach().permute(1, 2, 0)).astype(np.uint8)),
-            v2.Lambda(lambda x: np.array(x / 127.5 - 1).astype(np.float32))
+            v2.GaussianBlur(kernel_size=1, sigma=(0.1, 0.35)),
+            v2.ToDtype(dtype=torch.float32, scale=True),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            v2.Lambda(self.to_ndarray)
         ])
         example['image'] = transform(image)
         
