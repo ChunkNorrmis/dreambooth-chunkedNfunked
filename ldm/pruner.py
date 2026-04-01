cfb59a1db809ea6b36c8f37dce465c30581e5811
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-01 14:43:41 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,12 +1,12 @@
-def prune_checkpoint(old_state, fp32=False):
+def prune_checkpoint(old_state, precision='float16'):
     if int(old_state['global_step']) > 0:
         print(f"This is global step {old_state['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in old_state.items() if k != "optimizer_states" and k != 'state_dict'}
-        if not fp32:
+        if precision == 'float16':
             pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in old_state['state_dict'].items()}
             pruned_checkpoint['precision'] = 'float16'
-        else:
+        elif precision == 'float32':
             pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in old_state['state_dict'].items()}
             pruned_checkpoint['precision'] = 'float32'
         
