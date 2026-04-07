93fcaff90655b4e5bfd3e31441e1b3194f3c7a78
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-06 23:31:03 -0400

    Update joepenna_dreambooth_config.py

--- dreambooth_helpers/joepenna_dreambooth_config.py
+++ dreambooth_helpers/joepenna_dreambooth_config.py
@@ -138,17 +138,15 @@ class JoePennaDreamboothConfigSchemaV1():
     def normal_data(self, training_data):
         mean = 0.
         std = 0.
-        n_imgs = self.training_images_count
-        batch_data = torch.randn(n_imgs, 3, self.res, self.res) 
         for train_data in training_data:
             data = decode_image(train_data, mode='RGB')
             crop = min(data.shape[1], data.shape[2])
             data = fun.center_crop(data, (crop, crop))
             data = fun.resize(data, size=(self.res, self.res), interpolation=3, antialias=True)
             data = fun.to_dtype(data, dtype=torch.float32, scale=True)
-            data = data.view(batch_data.size(0), data.size(0), -1)
-            mean += data.mean(2).sum(0)
-            std += data.std(2).sum(0)
+            data = data.view(data.size(0), -1)
+            mean += data.mean(1)
+            std += data.std(1)
         mean /= self.training_images_count
         std /= self.training_images_count
         return mean, std
