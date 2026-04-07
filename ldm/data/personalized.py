21a05b0e7b803efd0bc7805bb4b349e039cb9529
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-07 12:07:50 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -29,8 +29,8 @@ class PersonalizedBase(Dataset):
         self.coarse_class_text = coarse_class_text
         self.size = size
         self.reg = reg
-        self.mean = mean
-        self.std = std
+        self.mean = torch.tensor(mean, dtype=torch.float32)
+        self.std = torch.tensor(std, dtype=torch.float32)
 
         if per_image_tokens:
             assert self.num_images < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
