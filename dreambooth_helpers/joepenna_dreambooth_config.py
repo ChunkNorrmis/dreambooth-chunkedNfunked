e9d97d3f70ece75e7276b1a91e555f35938ba6c5
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-06 21:46:32 -0400

    Update joepenna_dreambooth_config.py

--- dreambooth_helpers/joepenna_dreambooth_config.py
+++ dreambooth_helpers/joepenna_dreambooth_config.py
@@ -113,8 +113,7 @@ class JoePennaDreamboothConfigSchemaV1():
 
         self.validate_gpu_vram()
         self._create_log_folders()
-        self.mean, self.std = self.normal_data(self.training_images)
-
+       
     def validate_gpu_vram(self):
         def convert_size(size_bytes):
             if size_bytes == 0:
@@ -132,20 +131,6 @@ class JoePennaDreamboothConfigSchemaV1():
         twenty_one_gigabytes = 22548578304
         if gpu_vram < twenty_one_gigabytes:
             raise Exception(f"VRAM: Currently unable to run on less than {convert_size(twenty_one_gigabytes)} of VRAM.")
-
-    def normal_data(self, training_data):
-        mean = 0.
-        std = 0.
-        n_samples = len(training_data)
-        for data in training_data:
-            data = decode_image(data, mode='RGB')
-            data = fun.to_dtype(data, dtype=torch.float32, scale=True)
-            data = data.view(data.size(0), -1)
-            mean += data.mean(1)
-            std += data.std(1)
-        mean /= n_samples
-        std /= n_samples
-        return mean, std
 
     def saturate_from_file(
             self,
