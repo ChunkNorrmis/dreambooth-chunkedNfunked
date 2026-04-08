aaf844d669000bf5e9ee56645c61d870f26b455b
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-08 14:31:13 -0400

    Update dreambooth_trainer_configurations.py

--- dreambooth_helpers/dreambooth_trainer_configurations.py
+++ dreambooth_helpers/dreambooth_trainer_configurations.py
@@ -163,7 +163,6 @@ def get_dreambooth_model_config(config: JoePennaDreamboothConfigSchemaV1) -> dic
 
 
 def get_dreambooth_data_config(config: JoePennaDreamboothConfigSchemaV1) -> dict:
-    mean, std = config.normalize_data()
     reg_block = {
         "target": "ldm.data.personalized.PersonalizedBase",
         "params": {
@@ -172,10 +171,10 @@ def get_dreambooth_data_config(config: JoePennaDreamboothConfigSchemaV1) -> dict
             "data_root": config.regularization_images_folder_path,
             "size": config.res,
             "repeats": 10,
-            "flip_p": config.flip_percent,
-            "placeholder_token": config.token,
+            "flip_p": 0.0,
+            "placeholder_token": '*',
             "coarse_class_text": config.class_word,
-            "token_only": config.token_only,
+            "token_only": False,
             "per_image_tokens": False,
             "center_crop": config.crop,
             "mixing_prob": 0.25
@@ -192,7 +191,6 @@ def get_dreambooth_data_config(config: JoePennaDreamboothConfigSchemaV1) -> dict
                 "target": "ldm.data.personalized.PersonalizedBase",
                 "params": {
                     "set": "train",
-                    "reg": False,
                     "data_root": config.training_images_folder_path,
                     "size": config.res,
                     "repeats": config.repeats,
@@ -203,7 +201,7 @@ def get_dreambooth_data_config(config: JoePennaDreamboothConfigSchemaV1) -> dict
                     "per_image_tokens": False,
                     "center_crop": config.crop,
                     "mixing_prob": 0.25,
-                    k: v for k, v in config.normalize_data()
+                    k: v for k, v in config.normalize_data().items()
                 }
             },
             "reg": reg_block if config.regularization_images_folder_path is not None and config.regularization_images_folder_path != '' else None,
@@ -211,11 +209,10 @@ def get_dreambooth_data_config(config: JoePennaDreamboothConfigSchemaV1) -> dict
                 "target": "ldm.data.personalized.PersonalizedBase",
                 "params": {
                     "set": "val",
-                    "reg": False,
                     "data_root": config.training_images_folder_path,
                     "size": config.res,
                     "repeats": 10,
-                    "flip_p": config.flip_percent,
+                    "flip_p": 0.0,
                     "placeholder_token": config.token,
                     "coarse_class_text": config.class_word,
                     "token_only": config.token_only,
