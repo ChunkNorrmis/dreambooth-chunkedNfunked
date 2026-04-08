733dc8e14e92e10ba1a2995b2a00ca2207868260
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-08 15:17:23 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -14,7 +14,7 @@ per_img_token_list = [
 
 class PersonalizedBase(Dataset):
     def __init__(self, set, data_root, size, repeats, flip_p, placeholder_token, coarse_class_text,
-                token_only, per_image_tokens, center_crop, mixing_prob, reg=False, mean=0, std=1):
+                token_only, per_image_tokens, center_crop, mixing_prob, reg=False, mean=torch.zeros(3), std=torch.ones(3)):
         
         self.data_root = data_root
         self.image_paths = find_images(self.data_root)
