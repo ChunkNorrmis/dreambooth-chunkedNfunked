4e3231bf9578094634d025a9dc38916044ae1cf0
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-09 20:26:22 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -47,7 +47,7 @@ class PersonalizedBase(Dataset):
             v2.RandomHorizontalFlip(p=self.chance),
             v2.GaussianBlur(kernel_size=1, sigma=(0.1, 0.3)),
             v2.ToDtype(dtype=torch.float32, scale=True),
-            v2.Normalize(mean=0.5, std=0.5),
+            v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
             v2.Lambda(lambda tt: np.array(tt.clone().detach().permute(1, 2, 0).cpu()).astype(np.float32)),
         ])
                 
