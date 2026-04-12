b67ef87c604f1288dd4544159c3a0542890202f9
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-12 09:05:54 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -1,11 +1,12 @@
 from pytorch_lightning.plugins.io.torch_plugin import TorchCheckpointIO
+import safetensors.torch.save_model
 
 from typing import Any, Callable, Dict, Optional
 from pytorch_lightning.utilities.types import _PATH
 from ldm.pruner import prune_checkpoint
  
 class PruningCheckpointIO(TorchCheckpointIO):
-    def __init__(self, precision='float16'):
+    def __init__(self, precision='float16', sftsr=False):
         self.precision = precision
     def save_checkpoint(
             self, 
@@ -14,4 +15,7 @@ class PruningCheckpointIO(TorchCheckpointIO):
             storage_options: Optional[Any] = None
         ) -> None:
         pruned_checkpoint = prune_checkpoint(checkpoint, precision=self.precision)
-        TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
+        if not sftsr:
+            TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
+        else:
+            safetensors.torch.save_model(pruned_checkpoint, path)
