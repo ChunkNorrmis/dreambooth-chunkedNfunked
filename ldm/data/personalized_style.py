8d8cbdede4ec71c0a2159d9ec50a535e68d636e1
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-13 02:59:47 -0400

    Update personalized_style.py

--- ldm/data/personalized_style.py
+++ ldm/data/personalized_style.py
@@ -1,8 +1,8 @@
 import os, torch, random, sys
 import numpy as np
 from torch.utils.data import Dataset
-from PIL import Image
 from torchvision.transforms import v2
+from torchvision.io import decode_image
 
 imagenet_templates_small = [
     'a painting in the style of {}',
@@ -68,7 +68,7 @@ class PersonalizedBase(Dataset):
         self.num_images = len(self.image_paths)
         self._length = self.num_images
         self.placeholder_token = placeholder_token
-        self.chance = flip_p
+        self.flip_p = flip_p
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
         self.size = size
@@ -83,21 +83,18 @@ class PersonalizedBase(Dataset):
         return self._length
 
     def __getitem__(self, i):
-        example = {}
         img_path = self.image_paths[i % self.num_images]
-        image = Image.open(img_path)
-        crop = min(image.size)
+        image = decode_image(img_path, mode='RGB')
+        crop = min(image.size(1), image.size(2))
         transform = v2.Compose([
-            v2.RGB(),
-            v2.PILToTensor(),
-            v2.ToDtype(dtype=torch.uint8, scale=True),
             v2.CenterCrop((crop, crop)),
             v2.Resize((self.size, self.size), interpolation=3, antialias=True),
             v2.RandomHorizontalFlip(p=self.flip_p),
             v2.GaussianBlur(kernel_size=1, sigma=0.2),
-            v2.Lambda(self.normpy)
+            v2.ToDtype(dtype=torch.float32, scale=True),
+            v2.Lambda(lambda x: ((x - 0.5) / 0.5).clone().detach().permute(1, 2, 0).cpu().numpy())
         ])
-        example['image'] = transform(image)
+        example = {'image': transform(image)}
         class_dir = os.path.dirname(img_path)
         im_class = os.path.basename(class_dir)
         token_dir = os.path.dirname(class_dir)
@@ -108,9 +105,3 @@ class PersonalizedBase(Dataset):
             example["caption"] = random.choice(imagenet_templates_small).format(im_token)
 
         return example
-
-    def normpy(self, x):
-        x = x.clone().detach().permute(1, 2, 0).numpy()
-        x = np.array(x).astype(np.float32)
-        return (x / 255 - 0.5) / 0.5
-
