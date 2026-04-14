79ee7a0222885ea2854356e91e64a7e7be952a1f
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-14 11:56:28 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -1,6 +1,6 @@
 import os
 from pytorch_lightning.plugins.io.torch_plugin import TorchCheckpointIO
-import safetensors.save_model
+from safetensors.torch import save_model
 
 from typing import Any, Callable, Dict, Optional
 from pytorch_lightning.utilities.types import _PATH
@@ -25,4 +25,4 @@ class PruningCheckpointIO(TorchCheckpointIO):
                 path = os.path.join(path, 'last.safetensors')
             else: 
                 path = f"{path}.safetensors"
-            safetensors.save_model(pruned_checkpoint, path)
+            save_model(pruned_checkpoint, path)
