c1325ab203c9cfc1b76180c13c3d74d226e3d0a3
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-15 11:40:44 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -1,9 +1,8 @@
 import os, torch, diffusers
 from safetensors.torch import save_file, load_file
 
-def depicklize(suspicious_pickle, device=None, only_weights=False):
-    if device is None:
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def depicklize(suspicious_pickle, output_folder, only_weights=False):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     if suspicious_pickle.endswith('.ckpt'):
         dict_pickle = torch.load(suspicious_pickle, map_location=device, weights_only=only_weights)
         if not only_weights:
@@ -13,7 +12,7 @@ def depicklize(suspicious_pickle, device=None, only_weights=False):
             metadata = {'format': 'ckpt'}
     pickle_dict = {k: v.contiguous() for k, v in dict_pickle['state_dict'].items()}
     nil_pickle = os.path.basename(suspicious_pickle).replace(f".{metadata['format']}", '.safetensors')
-    nil_pickle = os.path.join(os.path.dirname(suspicious_pickle), nil_pickle)
+    nil_pickle = os.path.join(output_folder, nil_pickle)
     save_file(pickle_dict, nil_pickle, metadata=metadata)
     loaded = load_file(nil_pickle, device=device)
     for k in pickle_dict.keys():
