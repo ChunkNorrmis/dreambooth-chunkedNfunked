af732ccad9680182be32d3716e1b9955f9ded0ba
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-16 10:14:59 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -1,23 +1,25 @@
 import os, torch, diffusers
 from safetensors.torch import save_file, load_file
 
-def depicklize(suspicious_pickle, out_path, only_weights=False):
+def depicklize(dict_pickle, out_path=None, only_weights=False):
     device = 'cpu'
-    if suspicious_pickle.endswith('.ckpt'):
-        dict_pickle = torch.load(suspicious_pickle, map_location=device, weights_only=only_weights)
+    suspicious_pickle = torch.load(dict_pickle, map_location=device, weights_only=only_weights)
         if not only_weights:
-            metadata = {k: f"{v}" for k, v in dict_pickle.items() if k != 'state_dict'}
+            metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
             metadata['format'] = 'ckpt'
         else:
             metadata = {'format': 'ckpt'}
-    pickle_dict = {k: v.contiguous() for k, v in dict_pickle['state_dict'].items()}
-    nil_pickle = os.path.basename(out_path).replace(f".{metadata['format']}", '.safetensors')
-    nil_pickle = os.path.join(os.path.dirname(out_path), nil_pickle)
-    save_file(pickle_dict, nil_pickle, metadata=metadata)
-    loaded = load_file(nil_pickle, device=device)
-    for k in pickle_dict.keys():
-        if not torch.equal(loaded[k], pickle_dict[k]):
+    hefty_pickle = {k: v.contiguous() for k, v in suspicious_pickle['state_dict'].items()}
+    if out_path is None:
+        out_path = dict_pickle
+    pickle_path, pickle = os.path.split(out_path)
+    nil_pickle = pickle.replace(f".{metadata['format']}", '.safetensors')
+    nil_pickle = os.path.join(pickle_path, nil_pickle)
+    save_file(hefty_pickle, nil_pickle, metadata=metadata)
+    dict_picholas = load_file(nil_pickle, device=device)
+    for k in hefty_pickle.keys():
+        pickeless = dict_picholas[k]
+        sus_pickle = hefty_pickle[k]
+        if not torch.equal(pickeless, sus_pickle):
             raise RuntimeError('keys do not match')
-        else:
-            print('conversion successful')
-            return nil_pickle
+    return nil_pickle
