42cca75acb62a91ed468472730877c1a80ed3452
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-16 16:34:35 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -14,11 +14,7 @@ class PruningCheckpointIO(TorchCheckpointIO):
     def save_checkpoint(self, checkpoint, path, storage_options=None):
         if self.safetensors:
             pruned_pickle, metadata = prune_pickle(checkpoint, precision=self.precision)
-            if path.endswith('.ckpt'):
-                pickle_path, pickle = os.path.split(path)
-                pickle = pickle.replace('.ckpt', '.safetensors')
-                nil_pickle = os.path.join(pickle_path, pickle)
-            else: nil_pickle = path
+            nil_pickle = f"{os.path.splitext(path)[0]}.safetensors"
             save_file(pruned_pickle, nil_pickle, metadata=metadata)
         else:
             pruned_checkpoint = prune_checkpoint(checkpoint, precision=self.precision)
