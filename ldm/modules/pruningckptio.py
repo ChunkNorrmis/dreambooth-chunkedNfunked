e9924a6a4fb4c82556e6d28ba12798c2e0294869
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-20 22:18:17 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -4,7 +4,7 @@ from pytorch_lightning.plugins.io.torch_plugin import TorchCheckpointIO
 from typing import Any, Callable, Dict, Optional
 from pytorch_lightning.utilities.types import _PATH
 from ldm.pruner import prune_checkpoint, prune_pickle
-import safetensors.torch
+import safetensors.torch import save_file, save_model
  
 class PruningCheckpointIO(TorchCheckpointIO):
     def __init__(self, precision='float16', format=None):
@@ -15,7 +15,7 @@ class PruningCheckpointIO(TorchCheckpointIO):
         path = f"{os.path.splitext(path)[0]}{self.format}"
         if self.format == '.safetensors':
             nil_pickle, metadata = prune_pickle(checkpoint, precision=self.precision)
-            safetensors.torch.save_file(nil_pickle, path, metadata=metadata)
+            save_model(nil_pickle, path, metadata=metadata)
         else:
             pruned_checkpoint = prune_checkpoint(checkpoint, precision=self.precision)
             TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
