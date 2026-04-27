2acb8dd113a25b1d857089decb4b44d3ae18b1e4
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-26 21:23:59 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -3,6 +3,7 @@ import numpy as np
 from typing import OrderedDict
 from torch.utils.data import Dataset
 from torchvision.transforms import v2
+from torchvision.transforms.v2 import functional as fun
 from torchvision.io import decode_image
 from captionizer import caption_from_path, generic_captions_from_path, find_images
 
