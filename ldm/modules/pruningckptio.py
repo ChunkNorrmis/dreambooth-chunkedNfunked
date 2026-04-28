9da26943c83fdeb2a607c81645f6a097cc88d112
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-28 19:58:27 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -12,14 +12,15 @@ class PruningCheckpointIO(TorchCheckpointIO):
         self.format = format
      
     def save_checkpoint(self, checkpoint, path, storage_options=None):
-     if not path.endswith(f".{self.format}"):
-         path, _ = os.path.splitext(path)
-         path = path + f".{self.format}"   
-     if self.format == 'safetensors':
+        if not path.endswith(f".{self.format}"):
+            path, _ = os.path.splitext(path)
+            path = path + f".{self.format}"   
+        if self.format == 'safetensors':
             nil_pickle, metadata = prune_pickle(checkpoint, precision=self.precision)
             save_file(nil_pickle, path, metadata=metadata)
         else:
             pruned_checkpoint = prune_checkpoint(checkpoint, precision=self.precision)
-            TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
+
+        TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
 
 
