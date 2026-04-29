2c5b985161b2a206855fe7baf5b82491a8441a88
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-28 22:10:56 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,24 +1,24 @@
-def prune_checkpoint(checkpoint, precision='float16'):
-    dtype = {'float16': half, 'float32': float}
+def prune_checkpoint(checkpoint, dtype='float16'):
+    precision = {'float16': half(), 'float32': float()}[dtype]
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != "optimizer_states" and k != 'state_dict'}
         pruned_checkpoint['dtype'] = precision
-        pruned_checkpoint['state_dict'] = {k: v.dtype[precision]().contiguous() for k, v in checkpoint['state_dict'].items()}
+        pruned_checkpoint['state_dict'] = {k: v.precision().contiguous() for k, v in checkpoint['state_dict'].items()}
                 
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
 
-def prune_pickle(checkpoint, precision='float16'):
-    dtype = {'float16': half, 'float32': float}
+def prune_pickle(checkpoint, dtype='float16'):
+    precision = {'float16': half(), 'float32': float()}[dtype]
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         metadata = {k: f"{v}" for k, v in checkpoint.items() if k != 'optimizer_states' and k != 'state_dict'}
         metadata['format'] = 'pt'
         metadata['dtype'] = precision
-        nil_pickle = {k: v.dtype[precision]().contiguous() for k, v in checkpoint['state_dict'].items()}
+        nil_pickle = {k: v.precision().contiguous() for k, v in checkpoint['state_dict'].items()}
         keys = [k for k in metadata.keys()]
         keys += 'state_dict'
         
