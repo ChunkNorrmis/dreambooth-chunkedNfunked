3f10d596b065547ee89e2428508f46c38bee4d18
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-28 22:31:09 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,24 +1,28 @@
 def prune_checkpoint(checkpoint, dtype='float16'):
-    precision = {'float16': lambda v: v.half().contiguous(), 'float32': lambda v: v.float().contiguous()}
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != "optimizer_states" and k != 'state_dict'}
         pruned_checkpoint['dtype'] = precision
-        pruned_checkpoint['state_dict'] = {k: precision[dtype](v) for k, v in checkpoint['state_dict'].items()}
-                
+        if dtype == 'float16':     pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
+        else:
+            pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
+
+      
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
 
 def prune_pickle(checkpoint, dtype='float16'):
-    precision = {'float16': lambda v: v.half().contiguous(), 'float32': lambda v: v.float().contiguous()
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         metadata = {k: f"{v}" for k, v in checkpoint.items() if k != 'optimizer_states' and k != 'state_dict'}
         metadata['format'] = 'pt'
         metadata['dtype'] = precision
-        nil_pickle = {k: precision[dtype](v) for k, v in checkpoint['state_dict'].items()}
+        if dtype == 'float16':
+            nil_pickle = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
+        else:
+            nil_pickle = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
         keys = [k for k in metadata.keys()]
         keys += 'state_dict'
         
