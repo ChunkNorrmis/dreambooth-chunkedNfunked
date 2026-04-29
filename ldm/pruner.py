ffa44d31f79fb39c915bc1992ee59923de1ddaad
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-28 22:50:24 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -4,10 +4,10 @@ def prune_checkpoint(checkpoint, dtype='float16'):
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != "optimizer_states" and k != 'state_dict'}
         pruned_checkpoint['dtype'] = dtype
-        if dtype == 'float16':     pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
-        else:
+        if dtype == 'float16':
             pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
-
+        else:
+            pruned_checkpoint['state_dict'] = {k: v.float().contiguous() for k, v in checkpoint['state_dict'].items()}
       
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
@@ -22,9 +22,9 @@ def prune_pickle(checkpoint, dtype='float16'):
         if dtype == 'float16':
             nil_pickle = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
         else:
-            nil_pickle = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
-        keys = [k for k in metadata.keys()]
-        keys += 'state_dict'
+            nil_pickle = {k: v.float().contiguous() for k, v in checkpoint['state_dict'].items()}
+        key_list = [k for k in metadata.keys()]
+        key_list += ['state_dict']
         
-        print(f"Checkpoint Keys: {keys}")
+        print(f"Checkpoint Keys: {key_list}")
         return nil_pickle, metadata
