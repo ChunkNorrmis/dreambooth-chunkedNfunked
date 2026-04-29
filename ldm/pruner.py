c01232e32eb12926a6816540b3a993db7e89ab21
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-29 16:24:19 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,5 +1,4 @@
 import os, sys, torch
-from safetensors.torch import save_file
 
 def prune_checkpoint(checkpoint, dtype='float16'):
     if int(checkpoint['global_step']) > 0:
@@ -14,24 +13,3 @@ def prune_checkpoint(checkpoint, dtype='float16'):
       
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
-
-
-def prune_pickle(checkpoint, dtype='float16'):
-    if int(checkpoint['global_step']) > 0:
-        print(f"This is global step {checkpoint['global_step']}.")
-        print('Removing optimizer states from checkpoint')
-        state_dict = {}
-        for k in checkpoint['state_dict'].keys():
-            state_dict[k] = checkpoint['state_dict'][k]
-        if dtype == 'float16':
-            nil_pickle = {k: v.half().contiguous() for k, v in state_dict.items()}
-        else:
-            nil_pickle = {k: v.contiguous() for k, v in state_dict.items()}
-        metadata = {k: f"{v}" for k, v in checkpoint.items() if (k != 'optimizer_states' and k != 'state_dict')}
-        metadata['format'] = 'pt'
-        metadata['precision'] = dtype
-        keys_list = [k for k in metadata.keys()]
-        keys_list += ['state_dict']
-         
-        print(f"Checkpoint Keys: {keys_list}")
-        return nil_pickle, metadata
