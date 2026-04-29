d227da890f02873f50ddb0ad3de233b1a0f5d371
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-29 08:18:35 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -16,13 +16,13 @@ def prune_pickle(checkpoint, dtype='float16'):
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
-    if dtype == 'float16':
-        nil_pickle = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
-    else:
-        nil_pickle = {k: v.float().contiguous() for k, v in checkpoint['state_dict'].items()}
-    metadata = {k: f"{checkpoint[k]}" for k in checkpoint.keys() if k != 'optimizer_states' and k != 'state_dict'}
-    metadata['format'] = 'pt'
-    metadata['precision'] = dtype
+        if dtype == 'float16':
+            nil_pickle = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
+        else:
+            nil_pickle = {k: v.float().contiguous() for k, v in checkpoint['state_dict'].items()}
+        metadata = {k: f"{checkpoint[k]}" for k in checkpoint.keys() if k != 'optimizer_states' and k != 'state_dict'}
+        metadata['format'] = 'pt'
+        metadata['precision'] = dtype
         keys_list = metadata.keys() + 'state_dict'
          
         print(f"Checkpoint Keys: {keys_list}")
