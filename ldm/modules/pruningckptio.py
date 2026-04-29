122e0b6008b4f498ca51378b14e4a071ae155cc6
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-29 16:26:00 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -3,24 +3,12 @@ from pytorch_lightning.plugins.io.torch_plugin import TorchCheckpointIO
 
 from typing import Any, Callable, Dict, Optional
 from pytorch_lightning.utilities.types import _PATH
-from ldm.pruner import prune_checkpoint, prune_pickle
-from safetensors.torch import save_file
+from ldm.pruner import prune_checkpoint
  
 class PruningCheckpointIO(TorchCheckpointIO):
-    def __init__(self, dtype='float16', format='safetensors'):
-        super().__init__()
-        self.dtype = dtype
-        self.format = format
+    def __init__(self):
      
     def save_checkpoint(self, checkpoint, path, storage_options=None):
-        if path.endswith('.safetensors'):
-            if not path.endswith(f".{self.format}"):
-                path = os.path.splitext(path)[0] + f".{self.format}"
-            nil_pickle, metadata = prune_pickle(checkpoint, dtype=self.dtype)
-            save_file(nil_pickle, path, metadata=metadata)
-        elif path.endswith('.ckpt'):
-            if not path.endswith(f".{self.format}"):
-                path = os.path.splitext(path)[0] + f".{self.format}"
-            pruned_checkpoint = prune_checkpoint(checkpoint, dtype=self.dtype)
-            TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
+        pruned_checkpoint = prune_checkpoint(checkpoint, dtype=self.dtype)
+        TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
 
