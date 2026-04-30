105207bf13d8f78df8a70ebdc3ee94bf884ffe23
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-30 11:10:43 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -4,12 +4,14 @@ def prune_checkpoint(checkpoint, dtype='float16'):
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
-        pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != 'optimizer_states' and k != 'state_dict'}
-        pruned_checkpoint['precision'] = dtype
+        if 'optimizer_states' in checkpoint.keys():
+            del checkpoint['optimizer_states']
         if dtype == 'float16':
-            pruned_checkpoint['state_dict'] = {k: v.half().contiguous() for k, v in checkpoint['state_dict'].items()}
+            pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != 'state_dict'}
+            pruned_checkpoint['state_dict'] = {k: v.half() for k, v in checkpoint['state_dict'].items()}
         else:
-            pruned_checkpoint['state_dict'] = {k: v.contiguous() for k, v in checkpoint['state_dict'].items()}
-      
+            pruned_checkpoint = checkpoint.copy()
+        pruned_checkpoint['precision'] = dtype
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
