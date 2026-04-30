608ae98c3d06dbeba5ff76d2b47e2ddfcaca1c6b
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-04-30 17:00:10 -0400

    Update joepenna_dreambooth_config.py

--- dreambooth_helpers/joepenna_dreambooth_config.py
+++ dreambooth_helpers/joepenna_dreambooth_config.py
@@ -116,7 +116,7 @@ class JoePennaDreamboothConfigSchemaV1():
         self.project_config_filename = f"{self.project_name}-config.json"
         
         if not os.path.exists(os.path.relpath(model_path)):
-            if model_path.startswith('https://huggingface.co') and not os.path.exists(os.path.basename(model_path):
+            if model_path.startswith('https://huggingface.co') and not os.path.exists(os.path.basename(model_path)):
                 self.model_path = self.get_model_from_hub(repo_url=model_path)    
             else: raise Exception(f"Model Path Not Found: '{model_path}'.")
         else: self.model_path = os.path.relpath(model_path)
