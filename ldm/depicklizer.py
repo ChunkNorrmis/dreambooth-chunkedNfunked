948f5a97dc708713e15f521a70baa08273f78ece
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-02 11:57:35 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -1,25 +1,22 @@
 import os, torch, diffusers
-from safetensors.torch import save_file, load_file
+from safetensors.torch import save_file, save, load
 
-def depicklize(dict_pickle, out_path=None, only_weights=False):
+def depicklize(dict_pickle, out_path=None):
     device = 'cpu'
-    suspicious_pickle = torch.load(dict_pickle, map_location=device, weights_only=only_weights)
-        if not only_weights:
-            metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
-            metadata['format'] = 'ckpt'
-        else:
-            metadata = {'format': 'ckpt'}
+    suspicious_pickle = torch.load(dict_pickle, map_location=torch.device(device), weights_only=False)
     hefty_pickle = {k: v.contiguous() for k, v in suspicious_pickle['state_dict'].items()}
+    metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
+    metadata['format'] = 'pt'
     if out_path is None:
-        out_path = dict_pickle
-    pickle_path, pickle = os.path.split(out_path)
-    nil_pickle = pickle.replace(f".{metadata['format']}", '.safetensors')
-    nil_pickle = os.path.join(pickle_path, nil_pickle)
-    save_file(hefty_pickle, nil_pickle, metadata=metadata)
-    dict_picholas = load_file(nil_pickle, device=device)
+        nil_pickle = os.path.splitext(dict_pickle)[0] + '.safetensors'
+    elif not os.path.isdir(out_path):
+        out_path = os.path.dirname(out_path)
+    nil_pickle = os.path.join(out_path, dict_pickle.replace('.ckpt', '.safetensors'))
+    savetensors = save(hefty_pickle)
+    dict_pickless = load(savetensors, device=device)
     for k in hefty_pickle.keys():
-        pickeless = dict_picholas[k]
+        pickless = dict_pickless[k]
         sus_pickle = hefty_pickle[k]
-        if not torch.equal(pickeless, sus_pickle):
+        if not torch.equal(pickless, sus_pickle):
             raise RuntimeError('keys do not match')
-    return nil_pickle
+    save_file(hefty_pickle, nil_pickle, metadata=metadata)
