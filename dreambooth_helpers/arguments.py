2b3d776fcc60ce75d59c2b5767f5f9dd85e27e06
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-01 23:25:22 -0400

    Refactor argument parsing in arguments.py
    Refactor argument parsing and update argument names.

--- dreambooth_helpers/arguments.py
+++ dreambooth_helpers/arguments.py
@@ -2,7 +2,6 @@ import argparse, random
 from dreambooth_helpers.joepenna_dreambooth_config import JoePennaDreamboothConfigSchemaV1
 
 
-@JoePennaDreamboothConfigSchemaV1
 def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
     def _get_parser():
         parser = argparse.ArgumentParser()
@@ -36,7 +35,7 @@ def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
             help="Train only using the token and no class."
         )
         parser.add_argument(
-            "--model_path",
+            "--training_model",
             type=str,
             required=False,
             default='https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt',
@@ -51,7 +50,7 @@ def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
         parser.add_argument(
             "--regularization_images",
             type=str,
-            required=True,
+            required=False,
             help="Path to directory with regularization images"
         )
         parser.add_argument(
@@ -74,7 +73,7 @@ def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
             "--learning_rate",
             type=float,
             required=False,
-            default=1e-06,
+            default=1.0e-06,
             help="Set the learning rate. Defaults to 1.0e-06 (0.000001).  Accepts scientific notation."
         )
         parser.add_argument(
@@ -128,13 +127,39 @@ def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
             help='saves model state dict as float32, rather than float16 (the default)'
         )
         parser.add_argument(
-            '--savetensors',
-            action='store_true',
-            help='saves model in safetensors format instead of a checkpoint file'
+            '--safetensors',
+            action='store_true'
         )
 
         return parser
 
     parser = _get_parser()
-    return parser.parse_known_args()
-        
+    opt, unknown = parser.parse_known_args()
+    config = JoePennaDreamboothConfigSchemaV1()
+
+    if opt.config_file_path is not None:
+        config.saturate_from_file(config_file_path=opt.config_file_path)
+    else:
+        config.saturate(
+            project_name=opt.project_name,
+            debug=opt.debug,
+            gpu=opt.gpu,
+            save_every_x_steps=opt.save_every_x_steps,
+            training_images_folder_path=opt.training_images,
+            regularization_images_folder_path=opt.regularization_images,
+            token=opt.token,
+            token_only=opt.token_only,
+            class_word=opt.class_word,
+            flip_percent=opt.flip_p,
+            learning_rate=opt.learning_rate,
+            model_path=opt.training_model,
+            repeats=opt.repeats,
+            batch_size=opt.batch_size,
+            accumed_grads=opt.accumed_grads,
+            res=opt.resolution,
+            crop=opt.center_crop,
+            precision=opt.fp32,
+            safetensors=opt.safetensors
+        )
+
+    return config
