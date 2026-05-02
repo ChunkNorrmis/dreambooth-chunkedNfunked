0ea7b73554922c63e3b42110fc8ef5bd8ea6a612
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-02 12:14:41 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -1,17 +1,16 @@
 import os, torch, diffusers
 from safetensors.torch import save_file, save, load
 
-def depicklize(dict_pickle, out_path=None):
+def depicklize(dict_pickle, nil_pickle=None):
     device = 'cpu'
     suspicious_pickle = torch.load(dict_pickle, map_location=torch.device(device), weights_only=False)
     hefty_pickle = {k: v.contiguous() for k, v in suspicious_pickle['state_dict'].items()}
     metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
     metadata['format'] = 'pt'
-    if out_path is None:
+    if nil_pickle is None:
         nil_pickle = os.path.splitext(dict_pickle)[0] + '.safetensors'
-    elif not os.path.isdir(out_path):
-        out_path = os.path.dirname(out_path)
-    nil_pickle = os.path.join(out_path, dict_pickle.replace('.ckpt', '.safetensors'))
+    elif os.path.isdir(nil_pickle):
+        nil_pickle = os.path.join(nil_pickle, dict_pickle.replace('.ckpt', '.safetensors'))
     savetensors = save(hefty_pickle)
     dict_pickless = load(savetensors, device=device)
     for k in hefty_pickle.keys():
