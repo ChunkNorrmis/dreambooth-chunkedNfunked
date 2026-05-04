41331a1a33852784364c9d5c4c51692172798a23
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-03 20:09:20 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -1,5 +1,5 @@
 import os, torch, diffusers, sys
-from safetensors.torch import save_file, save, load
+from safetensors.torch import save_file, save
 
 def depicklize(dict_pickle, nil_pickle=None):
     device = 'cpu'
@@ -11,8 +11,7 @@ def depicklize(dict_pickle, nil_pickle=None):
         nil_pickle = os.path.splitext(dict_pickle)[0] + '.safetensors'
     elif os.path.isdir(nil_pickle):
         nil_pickle = os.path.join(nil_pickle, dict_pickle.replace('.ckpt', '.safetensors'))
-    savetensors = save(hefty_pickle)
-    dict_pickless = load(savetensors)
+    dict_pickless = save(hefty_pickle)
     for k in hefty_pickle.keys():
         pickless = dict_pickless[k]
         sus_pickle = hefty_pickle[k]
