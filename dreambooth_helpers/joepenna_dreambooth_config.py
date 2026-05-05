96fff92a1232c0df4e3ebf327b7a97f5f55e6628
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-05 09:45:20 -0400

    Update joepenna_dreambooth_config.py

--- dreambooth_helpers/joepenna_dreambooth_config.py
+++ dreambooth_helpers/joepenna_dreambooth_config.py
@@ -1,6 +1,7 @@
 import os, json, math, glob, shutil, sys, torch, random
 from datetime import datetime, timezone
 from pytorch_lightning import seed_everything
+from ldm.download_model import get_model_from_hub
 
 
 class JoePennaDreamboothConfigSchemaV1():
@@ -196,17 +197,6 @@ class JoePennaDreamboothConfigSchemaV1():
             print(project_config_json)
             print(f"✅ {self.project_config_filename} successfully generated.  Proceed to training.")
 
-    def get_model_from_hub(self, repo_url=None):
-        from huggingface_hub.file_download import hf_hub_download
-        import hf_xet, hf_transfer
-        repo_id = f"{repo_url.split('/')[3]}/{repo_url.split('/')[4]}"
-        ckpt_file = os.path.basename(repo_url)
-        model_path = os.path.join(sys.path[0], ckpt_file)
-        if not os.path.exists(model_path):
-            print(f"Downloading '{ckpt_file}'")
-            hf_hub_download(repo_id, ckpt_file, local_dir=sys.path[0])
-        return os.path.relpath(model_path)
-        
     def get_training_folder_name(self) -> str:
         return f"{self.config_date_time}_{self.project_name}"
 
