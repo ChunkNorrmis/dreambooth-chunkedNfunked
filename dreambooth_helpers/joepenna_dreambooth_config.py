7d3954597955059c9c422aa914a287843c2fc17c
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-05 01:28:52 -0400

    Update joepenna_dreambooth_config.py

--- dreambooth_helpers/joepenna_dreambooth_config.py
+++ dreambooth_helpers/joepenna_dreambooth_config.py
@@ -200,12 +200,12 @@ class JoePennaDreamboothConfigSchemaV1():
         from huggingface_hub.file_download import hf_hub_download
         import hf_xet, hf_transfer
         repo_id = f"{repo_url.split('/')[3]}/{repo_url.split('/')[4]}"
-        model_ckpt = os.path.basename(repo_url)
-        default_path = os.path.join(sys.path[0], model_ckpt)
-        if not os.path.exists(default_path):
-            print(f"Downloading '{model_ckpt}'")
-            hf_hub_download(repo_id, model_ckpt, local_dir=sys.path[0])
-        return os.path.relpath(default_path)
+        ckpt_file = os.path.basename(repo_url)
+        model_path = os.path.join(sys.path[0], ckpt_file)
+        if not os.path.exists(model_path):
+            print(f"Downloading '{ckpt_file}'")
+            hf_hub_download(repo_id, ckpt_file, local_dir=sys.path[0])
+        return os.path.relpath(model_path)
         
     def get_training_folder_name(self) -> str:
         return f"{self.config_date_time}_{self.project_name}"
