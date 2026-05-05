932b68e413f1de95909519b91cf2b601a4446d60
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-05 00:53:36 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -17,8 +17,8 @@ class PersonalizedBase(Dataset):
         data_root=None,
         set='train',
         reg=False,
-        placeholder_token='rock',
-        coarse_class_text='lobster',
+        placeholder_token='lobster',
+        coarse_class_text=None,
         size=512,
         repeats=100,
         center_crop=False,
@@ -35,21 +35,23 @@ class PersonalizedBase(Dataset):
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
         self.mixing_prob = mixing_prob
-        self.flip_p = flip_p
         self.size = size
-        self.repeats = repeats
         self.reg = reg
         self.placeholder_token = placeholder_token
         self.coarse_class_text = coarse_class_text
-        self.nml = torch.tensor([0.5,0.5,0.5])
+        self.tt_nml = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float32)
+        self.np_nml = np.array([0.5, 0.5, 0.5], dtype=np.float32)
+        self.scl = np.array([255, 255, 255], dtype=np.float32)
 
         if per_image_tokens:
             assert self.n_imgs < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
         if set == 'train':
-            self._length = self.n_imgs * self.repeats
+            self._length = self.n_imgs * repeats
+            self.flip_p = flip_p
 
         if self.reg:
+            self.flip_p = 0.0
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
 
 
@@ -59,25 +61,14 @@ class PersonalizedBase(Dataset):
     def __getitem__(self, i):
         example = {}
         img_path = self.imgs[i % self.n_imgs]
-        #transform = v2.Compose([
-        #    v2.RGB(),
-        #    v2.PILToTensor(),
-        #    v2.ToDtype(dtype=torch.uint8, scale=True),
-        #    v2.Lambda(self.crop_and_resize),
-        #    v2.RandomHorizontalFlip(p=self.flip_p),
-        #    v2.GaussianBlur(kernel_size=1, sigma=0.2),
-        #    v2.ToDtype(dtype=torch.float32, scale=True),
-        #    v2.Normalize(mean=self.nml, std=self.nml),
-        #    v2.Lambda(self.numpify)
-        #])
         image = self.transformer(img_path)
-        if self.reg:
-            example['caption'] = generic_captions_from_path(self.imgs[i % self.n_imgs], self.data_root, self.reg_tokens)
-        else:
-            example['caption'] = caption_from_path(self.imgs[i % self.n_imgs], self.data_root, self.coarse_class_text, self.placeholder_token)
-        
         example['image'] = image
+        if self.reg:
+            example['caption'] = generic_captions_from_path(img_path, self.data_root, self.reg_tokens)
+        else:
+            example['caption'] = caption_from_path(img_path, self.data_root, self.coarse_class_text, self.placeholder_token)
         return example
+
 
     def transformer(self, img_path):
         image = cv2.imread(img_path)
@@ -91,10 +82,21 @@ class PersonalizedBase(Dataset):
             image = cv2.resize(image, dsize=(self.size, self.size), interpolation=interp)
         if random.random() < self.flip_p:
             image = cv2.flip(image, 1)
-        image = (np.array(image).astype(np.float32) / 255 - 0.5) / 0.5
-        return image
+        return (image.astype(np.float32) / self.scl - self.np_nml) / self.np_nml
 
-    def numpify(self, x): return x.permute(2,1,0).permute(1,0,2).cpu().numpy(force=True)
+    def transform(self, img_path):
+        image = decode_image(img_path, mode='RGB')
+        transform = v2.Compose([
+            v2.Lambda(self.crop_and_resize),
+            v2.RandomHorizontalFlip(p=self.flip_p),
+            v2.GaussianBlur(kernel_size=1, sigma=0.2),
+            v2.ToDtype(dtype=torch.float32, scale=True),
+            v2.Normalize(mean=self.tt_nml, std=self.tt_nml),
+            v2.Lambda(self.numpify)
+        ])
+        return transform(image)
+    
+    def numpify(self, x): return x.permute(2,1,0).permute(1,0,2).cpu().numpy(force=True).astype(np.float32)
 
     def crop_and_resize(self, x):
         h, w = x.size(1), x.size(2)
