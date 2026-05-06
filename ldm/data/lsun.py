eadb29e28b2a30d45553d5588c75c15f7ace5ce2
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-06 10:26:20 -0400

    Update lsun.py

--- ldm/data/lsun.py
+++ ldm/data/lsun.py
@@ -1,47 +1,50 @@
-import os, random, torch, sys
+import os, torch, sys, cv2
+from random import random
 import numpy as np
 from torch.utils.data import Dataset
-from torchvision.transforms import v2
-from PIL import Image
 
 
 class LSUNBase(Dataset):
     def __init__(self, txt_file, data_root, size=512, interpolation="bicubic", flip_p=0.5):
-        super().__init__()
         self.data_paths = txt_file
         self.data_root = data_root
         with open(self.data_paths, "r") as f:
             self.image_paths = f.read().splitlines()
         self._length = len(self.image_paths)
+        self.size = size
+        self.flip_p = flip_p
         self.labels = {
             "relative_file_path_": [l for l in self.image_paths],
-            "file_path_": [os.path.join(self.data_root, l)
-                           for l in self.image_paths],
+            "file_path_": [os.path.join(self.data_root, l) for l in self.image_paths]
         }
-        self.chance = flip_p
-        self.size = size
+
 
     def __len__(self):
         return self._length
 
+
     def __getitem__(self, i):
         example = dict((k, self.labels[k][i]) for k in self.labels)
-        image = Image.open(example["file_path_"])
-        crop = min(image.size)
-        transform = v2.Compose([
-            v2.RGB(),
-            v2.PILToTensor(),
-            v2.ToDtype(dtype=torch.uint8, scale=True),
-            v2.CenterCrop((crop, crop)),
-            v2.Resize((self.size, self.size), interpolation=3, antialias=True),
-            v2.RandomHorizontalFlip(p=self.chance),
-            v2.GaussianBlur(kernel_size=1, sigma=(0.1, 0.3)),
-            v2.ToDtype(dtype=torch.float32),
-            v2.Lambda(lambda x: ((x / 255 - 0.5) / 0.5).permute(1, 2, 0).contiguous().numpy())
-        ])
-        example['image'] = transform(image)
-
+        image = self.transform(example["file_path_"])
+        example['image'] = image
         return example
+
+
+    def transform(self, img_path):
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        h, w = image.shape[0], image.shape[1]
+        crop = min(h, w)
+        if self.center_crop and h != w:
+            image = image[(h - crop) // 2: (h + crop) // 2, (w - crop) // 2: (w + crop) // 2]
+        if self.size != crop:
+            interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
+            image = cv2.resize(image, dsize=(self.size, self.size), interpolation=interp)
+        if self.flip_p > random():
+            image = cv2.flip(image, 1)
+        image = cv2.GaussianBlur(image, ksize=(1, 1), sigmaX=0.2, sigmaY=0.2)
+        image = ((image / 255. - 0.5) / 0.5).astype(np.float32)
+        return image
 
 
 class LSUNChurchesTrain(LSUNBase):
@@ -75,3 +78,4 @@ class LSUNCatsValidation(LSUNBase):
     def __init__(self, flip_p=0., **kwargs):
         super().__init__(txt_file="data/lsun/cat_val.txt", data_root="data/lsun/cats",
                          flip_p=flip_p, **kwargs)
+
