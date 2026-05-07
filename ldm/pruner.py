ada57a4935267234225c76296a8cd4c493854075
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-07 01:14:43 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -4,7 +4,7 @@ def prune_checkpoint(checkpoint, dtype='float16'):
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
-        pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != 'state_dict' and k != 'optimizer_states'}
+        pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ('state_dict', 'optimizer_states', 'callbacks', 'lr_schedulers')}
         if dtype == 'float16':
             pruned_checkpoint['state_dict'] = {k: v.half() for k, v in checkpoint['state_dict'].items()}
         else:
