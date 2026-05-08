0af7406ef73f7ce01fe3a6cd492433492afdd0eb
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-08 12:51:01 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -9,13 +9,12 @@ def depicklize(dict_pickle, nil_pickle=None):
     metadata['format'] = 'pt'
     savedtensors = sftr.save(hefty_pickle)
     dict_pickless = sftr.load(savedtensors)
-    for k in hefty_pickle.keys():
-        pickless = dict_pickless[k]
-        sus_pickle = hefty_pickle[k]
-        if not torch.equal(pickless, sus_pickle):
+    for k in suspicious_pickle['state_dict'].keys():
+        if not torch.equal(dict_pickless[k], suspicious_pickle['state_dict'][k]):
             raise RuntimeError('keys do not match')
     if nil_pickle is None:
         nil_pickle = dict_pickle.replace('.ckpt', '.safetensors')
     elif os.path.isdir(nil_pickle):
-        nil_pickle = os.path.join(nil_pickle, dict_pickle.replace('.ckpt', '.safetensors'))
+        dict_pickle = dict_pickle.replace('.ckpt', '.safetensors')
+        nil_pickle = os.path.join(nil_pickle, dict_pickle)
     sftr.save_file(hefty_pickle, nil_pickle, metadata=metadata)
