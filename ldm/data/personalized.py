b222ab25d045594bb11e3b4983f8713324018695
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-09 23:54:17 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -4,9 +4,7 @@ from torch.utils.data import Dataset
 from captionizer import caption_from_path, generic_captions_from_path, find_images
 
 
-per_img_token_list = [
-    'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',
-]
+per_img_token_list = ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת']
 
 class PersonalizedBase(Dataset):
     def __init__(
@@ -38,6 +36,7 @@ class PersonalizedBase(Dataset):
         self.placeholder_token = placeholder_token
         self.coarse_class_text = coarse_class_text
         self.flip_p = flip_p
+        
         if per_image_tokens:
             assert self.n_imgs < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
@@ -45,13 +44,10 @@ class PersonalizedBase(Dataset):
             self._length = self.n_imgs * repeats
 
         if self.reg:
-            self.flip_p = 0.0
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
-
 
     def __len__(self):
         return self._length
-
 
     def __getitem__(self, i):
         example = {}
@@ -66,7 +62,6 @@ class PersonalizedBase(Dataset):
         example['image'] = image
         return example
 
-
     def transform(self, img_path):
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -79,7 +74,7 @@ class PersonalizedBase(Dataset):
             image = cv2.resize(image, dsize=(self.size, self.size), interpolation=interp)
         if random.random() < self.flip_p:
             image = cv2.flip(image, 1)
-        image = cv2.GaussianBlur(image, ksize=(1, 1), sigmaX=0.2, sigmaY=0.2)
+        image = cv2.GaussianBlur(image, ksize=(3, 3), sigmaX=0.2, sigmaY=0.2)
         image = ((image / 255. - 0.5) / 0.5).astype(np.float32)
         return image
 
