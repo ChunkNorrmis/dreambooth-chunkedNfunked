f7381daca92ac30b802bd2d7d11b10d14dee35f6
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-12 14:50:12 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -12,21 +12,19 @@ class PersonalizedBase(Dataset):
         data_root=None,
         set='train',
         reg=False,
-        placeholder_token='lobster',
-        coarse_class_text=None,
+        placeholder_token='carbuncle',
+        coarse_class_text='_',
         size=512,
         repeats=100,
         center_crop=True,
-        flip_p=0.5,
+        flip_p=0.0,
         mixing_prob=0.25,
         token_only=False,
         per_image_tokens=False
     ):
-        self.set = set
-        self.data_root = data_root
-        self.imgs = find_images(self.data_root)
+        self.imgs = find_images(data_root)
         self.n_imgs = len(self.imgs)
-        self._length = self.n_imgs
+        self._length = self.n_imgs * repeats
         self.token_only = token_only
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
@@ -40,8 +38,8 @@ class PersonalizedBase(Dataset):
         if per_image_tokens:
             assert self.n_imgs < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
-        if self.set == 'train':
-            self._length = self.n_imgs * repeats
+        #if set == 'train':
+         #   self._length = self.n_imgs * repeats
 
         if self.reg:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
@@ -52,19 +50,7 @@ class PersonalizedBase(Dataset):
     def __getitem__(self, i):
         example = {}
         img_path = self.imgs[i % self.n_imgs]
-        image = cv2.imread(img_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        h, w = image.shape[0], image.shape[1]
-        crop = min(h, w)
-        if self.center_crop and h != w:
-            image = image[(h - crop) // 2: (h + crop) // 2, (w - crop) // 2: (w + crop) // 2]
-        if self.size != crop:
-            interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
-            image = cv2.resize(image, dsize=(self.size, self.size), interpolation=interp)
-        if self.flip_p > random.random():
-            image = cv2.flip(image, 1)
-        image = cv2.GaussianBlur(image, ksize=(1, 1), sigmaX=0.2, sigmaY=0.2)
-        image = ((image / 255. - 0.5) / 0.5).astype(np.float32)
+        image = self.transformer(img_path)        
 
         if self.reg:
             example['caption'] = generic_captions_from_path(img_path, self.data_root, self.reg_tokens)
@@ -73,4 +59,34 @@ class PersonalizedBase(Dataset):
 
         example['image'] = image
         return example
+
+
+    def transformer(self, img_path):
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = self.crop_and_resize(img)
+        img = self.mirror(img)
+        img = self.blur(img)
+        return ((img // 255. - 0.5) // 0.5).astype(np.float32)
+    
+    def mirror(self, img):
+        if self.flip_p > 0.0:
+            if random.random() < self.flip_p:
+                img = cv2.flip(img, 1)
+        return img
+
+    def blur(self, img):
+        k = random.choice([1, 3, 5])
+        sig = random.randrange(1, 6) // 10
+        return cv2.GaussianBlur(img, ksize=(k, k), sigmaX=sig, sigmaY=sig)
+
+    def crop_and_resize(self, img):
+        h, w = img.shape[0], img.shape[1]
+        crop = min(h, w)
+        if self.center_crop and h != w:
+            img = img[(h - crop) // 2: (h + crop) // 2, (w - crop) // 2: (w + crop) // 2]
+        if self.size != crop:
+            interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
+            img = cv2.resize(img, dsize=(self.size, self.size), interpolation=interp)
+        return img
 
