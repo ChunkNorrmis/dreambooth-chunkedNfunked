34e16ee396bd0d85b8c1f963a78cce846251114d
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-18 10:03:22 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -1,26 +1,17 @@
-import os, sys, torch, cv2, random, numpy as np
+import os, sys, torch, cv2, random
+import numpy as np
 from typing import OrderedDict
 from torch.utils.data import Dataset
 from captionizer import caption_from_path, generic_captions_from_path, find_images
+
 
 
 per_img_token_list = ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת']
 
 class PersonalizedBase(Dataset):
     def __init__(
-        self,
-        data_root=None,
-        set='train',
-        reg=False,
-        placeholder_token='carbuncle',
-        coarse_class_text='_',
-        size=512,
-        repeats=100,
-        center_crop=True,
-        flip_p=0.5,
-        mixing_prob=0.25,
-        token_only=False,
-        per_image_tokens=False
+        self, data_root=None, set='train', reg=False, placeholder_token='carbuncle', coarse_class_text='_', size=512,
+        repeats=100, center_crop=True, flip_p=0.5, mixing_prob=0.25, token_only=False, per_image_tokens=False
     ):
         self.data_root = data_root
         self.imgs = find_images(data_root)
@@ -35,7 +26,6 @@ class PersonalizedBase(Dataset):
         self.placeholder_token = placeholder_token
         self.coarse_class_text = coarse_class_text
         self.flip_p = flip_p
-        
         if per_image_tokens:
             assert self.n_imgs < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
         if set == 'train':
@@ -43,8 +33,10 @@ class PersonalizedBase(Dataset):
         if self.reg:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
 
+
     def __len__(self):
         return self._length
+
 
     def __getitem__(self, i):
         img_path = self.imgs[i % self.n_imgs]
@@ -56,26 +48,30 @@ class PersonalizedBase(Dataset):
         example = {'caption': caption, 'image': image}
         return example
 
+
     def augment(self, img_path):
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = self.crop_and_resize(img)
-        img = self.mirror(img)
-        img = self.blur(img)
+        img = self.random_mirror(img)
+        img = self.random_blur(img)
         image = np.array(((img / 255. - 0.5) * 2.), dtype=np.float32)
         return image
 
-    def mirror(self, img):
+
+    def random_mirror(self, img):
         if random.random() < self.flip_p:
             img = cv2.flip(img, 1)
         return img
 
-    def blur(self, img):
+
+    def random_blur(self, img):
         if random.random() < 0.5:
-            knl = random.choice([1, 3])
-            sig = random.uniform(0.1, 0.5)
-            img = cv2.GaussianBlur(img, ksize=(knl, knl), sigmaX=sig, sigmaY=sig)
+            k = random.choice([1, 3, 1])
+            x = random.uniform(0.2, 0.5)
+            img = cv2.GaussianBlur(img, ksize=(k,k), sigmaX=x, sigmaY=x)
         return img
+
 
     def crop_and_resize(self, img):
         h, w = img.shape[:2]
@@ -86,4 +82,5 @@ class PersonalizedBase(Dataset):
             interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
             img = cv2.resize(img, dsize=(self.size, self.size), interpolation=interp)
         return img
+
 
