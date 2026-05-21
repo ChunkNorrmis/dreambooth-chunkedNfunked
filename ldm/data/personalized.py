a4fa8bf7f39d4934f1931b2320ec77486a4d0dde
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-21 14:10:18 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -69,10 +69,9 @@ class PersonalizedBase(Dataset):
 
 
     def rdm_blur(self, img):
-        if random.random() < 0.5:
-            k = random.randrange(2, 6) * 2 - 1
-            x = random.uniform(0.5, 1.0)
-            img = cv2.GaussianBlur(img, ksize=(k,k), sigmaX=x, sigmaY=x)
+        k = random.randrange(1, 6) * 2 - 1
+        x = random.randrange(0, 11) / 10
+        img = cv2.GaussianBlur(img, ksize=(k, k), sigmaX=x, sigmaY=x)
         return img
 
 
