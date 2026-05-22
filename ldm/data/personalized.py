6583aa811c6861efbed165e37479bbecd89747be
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-22 04:12:50 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -67,7 +67,7 @@ class PersonalizedBase(Dataset):
 
     def blur(self, image):
         if random.random() < 0.5:
-            k = random.randrange(2, 6) * 2 - 1
+            k = random.randrange(1, 6) * 2 - 1
             image = cv2.GaussianBlur(image, ksize=(k, k), sigmaX=0.5, sigmaY=0.5)
         return image
 
