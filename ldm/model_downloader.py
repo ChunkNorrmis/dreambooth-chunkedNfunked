f4e56ee0ae5bc7605aa2974b594f7d8cd35e136a
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-23 14:55:12 -0400

    Update and rename model_download.py to model_downloader.py

---
+++ ldm/model_downloader.py
@@ -0,0 +1,34 @@
+import os, sys
+from huggingface_hub.file_download import hf_hub_download
+import hf_xet, hf_transfer
+import gdown
+		
+class DownloadModel():
+	def __init__(self):
+		self.url = url
+		if self.url.startswith('https://huggingface.co'):
+			return self.from_huggingface_hub()
+		elif model_path.startswith('https://drive.google.com'):
+			return self.from_google_drive()
+
+
+	def from_huggingface_hub(self):
+		repo_id = f"{self.url.split('/')[3]}/{self.url.split('/')[4]}"
+		ckpt_file = os.path.basename(self.url)
+		model_path = os.path.join(sys.path[0], ckpt_file)
+		if not os.path.exists(model_path):
+			print(f"Downloading '{ckpt_file}'")
+			hf_hub_download(repo_id, ckpt_file, local_dir=sys.path[0])
+		return os.path.relpath(model_path)
+
+
+	def from_google_drive(self):
+		def on_progress(bytes_so_far: int, bytes_total: int | None) -> None:
+		    if bytes_total is not None:
+        		print(f"\r{bytes_so_far / bytes_total * 100:.1f}%", end="")
+
+		model_path = os.path.join(sys.path[0], 'model.ckpt')
+		if not os.path.exists(model_path):
+			print(f"Downloading '{os.path.basename(model_path)}'")
+			gdown.download(url=self.url, output=model_path, quiet=True, progress=on_progress)
+		return os.path.relpath(model_path)
