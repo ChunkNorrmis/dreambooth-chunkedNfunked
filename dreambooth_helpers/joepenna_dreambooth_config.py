508ba761f0a47a4a8d6af31baf4c0bbff7dd80c9
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-23 23:54:25 -0400

    Refactor model path handling in configuration

--- dreambooth_helpers/joepenna_dreambooth_config.py
+++ dreambooth_helpers/joepenna_dreambooth_config.py
@@ -113,11 +113,10 @@ class JoePennaDreamboothConfigSchemaV1():
         self.project_config_filename = f"{self.project_name}-config.json"
         
         if not os.path.exists(model_path):
-            if model_path.startswith('https://'):
-                if model_path.startswith('https://huggingface.co'):
-			        self.model_path = DownloadModel.from_huggingface_hub(model_path)
-		        elif model_path.startswith('https://drive.google.com'):
-			        self.model_path = DownloadModel.from_google_drive(model_path)
+			if model_path.startswith('https://huggingface.co'):
+				self.model_path = DownloadModel.from_huggingface_hub(model_path)
+			elif model_path.startswith('https://drive.google.com'):
+				self.model_path = DownloadModel.from_google_drive(model_path)
             else: raise Exception(f"Model Path Not Found: '{model_path}'.")
         else: self.model_path = os.path.relpath(model_path)
             
