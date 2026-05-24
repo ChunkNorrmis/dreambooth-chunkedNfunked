0720256593ff7a18498cdb397da07fb9a8490ce8
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-24 00:05:16 -0400

    Update model_downloader.py

--- ldm/model_downloader.py
+++ ldm/model_downloader.py
@@ -2,27 +2,25 @@ import os, sys
 from huggingface_hub.file_download import hf_hub_download
 import hf_xet, hf_transfer
 import gdown
-		
-class DownloadModel():
-    def __init__(self):
-		
-    def from_huggingface_hub(self, url):
-        repo_id = f"{url.split('/')[3]}/{url.split('/')[4]}"
-        ckpt_file = os.path.basename(url)
-        model_path = os.path.join(sys.path[0], ckpt_file)
-        if not os.path.exists(model_path):
-            print(f"Downloading '{ckpt_file}'")
-            hf_hub_download(repo_id, ckpt_file, local_dir=sys.path[0])
-        return os.path.relpath(model_path)
 
 
-    def from_google_drive(self, url):
-        def on_progress(bytes_so_far: int, bytes_total: int | None) -> None:
-            if bytes_total is not None:
-                print(f"\r{bytes_so_far / bytes_total * 100:.1f}%", end="")
+def from_huggingface_hub(url):
+    repo_id = f"{url.split('/')[3]}/{url.split('/')[4]}"
+    ckpt_file = os.path.basename(url)
+    model_path = os.path.join(sys.path[0], ckpt_file)
+    if not os.path.exists(model_path):
+        print(f"Downloading '{ckpt_file}'")
+        hf_hub_download(repo_id, ckpt_file, local_dir=sys.path[0])
+    return os.path.relpath(model_path)
 
-        model_path = os.path.join(sys.path[0], 'model.ckpt')
-        if not os.path.exists(model_path):
-            print(f"Downloading '{os.path.basename(model_path)}'")
-            gdown.download(url=url, output=model_path, quiet=True, progress=on_progress)
-        return os.path.relpath(model_path)
+
+def from_google_drive(url):
+    def on_progress(bytes_so_far: int, bytes_total: int | None) -> None:
+        if bytes_total is not None:
+            print(f"\r{bytes_so_far / bytes_total * 100:.1f}%", end="")
+
+    model_path = os.path.join(sys.path[0], 'model.ckpt')
+    if not os.path.exists(model_path):
+        print(f"Downloading '{os.path.basename(model_path)}'")
+        gdown.download(url=url, output=model_path, quiet=True, progress=on_progress)
+    return os.path.relpath(model_path)
