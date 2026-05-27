24a7e109b33e5e4c853b995b289f0fd62c163402
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-27 14:21:41 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -68,8 +68,9 @@ class PersonalizedBase(Dataset):
     def blur(self, image):
         if random.random() < 0.5:
             k = random.randrange(2, 6) * 2 - 1
-            r = random.uniform(0.5, 1.0)
-            image = cv2.GaussianBlur(image, ksize=(k,k), sigmaX=r, sigmaY=r)
+            r = random.uniform(0.5, 1.0) 
+            s = random.choice([r, 0])
+            image = cv2.GaussianBlur(image, ksize=(k, k), sigmaX=r, sigmaY=r)
         return image
 
     def crop_and_resize(self, image):
