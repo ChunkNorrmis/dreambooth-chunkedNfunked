4d55f59fdae997e613a3dac6b6b37d8fef6b556a
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-05-28 12:54:33 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,15 +1,12 @@
 import os, sys, torch
 
-def prune_checkpoint(checkpoint, dtype='float16'):
+def prune_checkpoint(checkpoint, float32=False):
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ['state_dict', 'optimizer_states', 'callbacks', 'lr_schedulers']}
-        if dtype == 'float16':
-            pruned_checkpoint['state_dict'] = {k: v.half() for k, v in checkpoint['state_dict'].items()}
-        else:
-            pruned_checkpoint['state_dict'] = {k: v for k, v in checkpoint['state_dict'].items()}
-        pruned_checkpoint['precision'] = dtype
+        pruned_checkpoint['state_dict'] = {k: v if float32 else v.half() for k, v in checkpoint['state_dict'].items()}
+        pruned_checkpoint['precision'] = 'fp32' if float32 else 'fp16'
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
