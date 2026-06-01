2a7de5f3819311a1601c00c13ebebbe2082cdedd
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-06-01 11:10:00 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -44,7 +44,8 @@ class PersonalizedBase(Dataset):
         img = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
         img = self.crop_and_resize(img)
         img = self.mirror(img)
-        img = random.choice([self.blur(img), self.sharpen(img)])
+        img = self.sharpen(img)
+        img = self.blur(img)
         image = img.astype(np.float32)
         image = (image / 255. - 0.5) * 2.
 
@@ -72,8 +73,12 @@ class PersonalizedBase(Dataset):
 
     def sharpen(self, img):
         if random.random() < 0.5:
-            blur = cv2.GaussianBlur(img, ksize=(3, 3), sigmaX=0.5, sigmaY=0.5)
-            img = cv2.addWeighted(img, alpha=1.1, src2=blur, beta=-0.1, gamma=0.0)
+            r = [n / 10. for n in range(5, 11)]
+            sig = random.choice(r)
+            alpha = 1 / sig
+            beta = 1 - alpha
+            blur = cv2.GaussianBlur(img, ksize=(3, 3), sigmaX=sig, sigmaY=sig)
+            img = cv2.addWeighted(img, alpha=alpha, src2=blur, beta=beta, gamma=0)
         return img
 
 
