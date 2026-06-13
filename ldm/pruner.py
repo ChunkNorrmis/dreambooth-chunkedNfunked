2187fb36169ccc0c8b393f5b20b759daaf35a138
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-06-13 08:40:53 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -5,7 +5,8 @@ def prune_checkpoint(checkpoint, float32=False):
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k != 'state_dict' and k != 'optimizer_states'}
-        pruned_checkpoint['state_dict'] = {k: v if float32 else v.half() for k, v in checkpoint['state_dict'].items()}
+        state_dict = {k: v if float32 else v.half() for k, v in checkpoint['state_dict'].items()}
+        pruned_checkpoint['state_dict'] = state_dict
         pruned_checkpoint['precision'] = 'fp32' if float32 else 'fp16'
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
