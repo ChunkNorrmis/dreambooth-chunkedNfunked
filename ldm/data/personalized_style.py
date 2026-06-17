e9a09ea4748a5c1798486f186b27b5c97aaac014
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-06-17 14:04:44 -0400

    Update personalized_style.py

--- ldm/data/personalized_style.py
+++ ldm/data/personalized_style.py
@@ -83,18 +83,19 @@ class PersonalizedBase(Dataset):
 
 
     def __getitem__(self, i):
-        img_path = self.imgs[i % self.n_imgs]        
+        img_path = self.imgs[i % self.n_imgs]
+        image = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
+        image = self.crop_and_resize(image)
+        image = self.mirror(image)
+        image = random.choice([self.blur, self.sharpen])(image)
+        image = np.array(image).astype(np.float32)
+        image = (image  / 255. - 0.5) * 2.
         if self.per_image_tokens and random.random() < self.mixing_prob:
             caption = random.choice(imagenet_dual_templates_small).format(self.placeholder_token, per_img_token_list[i % self.n_imgs])
         else:
             caption = random.choice(imagenet_templates_small).format(self.placeholder_token)
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
-        img = self.crop_and_resize(img)
-        img = self.mirror(img)
-        img = self.blur(img)
-        image = img.astype(np.float32)
-        image = (image / 255. - 0.5) * 2.
-        return {'caption': cap, 'image': image}
+        example = {'caption': caption, 'image': image}
+        return example
 
 
     def mirror(self, img):
@@ -105,10 +106,19 @@ class PersonalizedBase(Dataset):
 
     def blur(self, img):
         if random.random() < 0.5:
-            k = random.randrange(2, 5) * 2 - 1
-            r = [(n / 10) for n in range(5, 10)] + [0, 1.]
+            r = [n / 10 for n in range(6, 11)] + [0]
             sig = random.choice(r)
-            img = cv2.GaussianBlur(img, ksize=(k, k), sigmaX=sig, sigmaY=sig)
+            img = cv2.GaussianBlur(img, ksize=(3, 3), sigmaX=sig, sigmaY=sig)
+        return img
+
+
+    def sharpen(self, img):
+        if random.random() < 0.5:
+            mask = cv2.GaussianBlur(img, ksize=(5, 5), sigmaX=0, sigmaY=0)
+            alpha = 1.3
+            beta = 1 - alpha
+            sharpened = cv2.addWeighted(img, alpha=alpha, src2=mask, beta=beta, gamma=0.0)
+            img = cv2.GaussianBlur(sharpened, ksize=(3, 3), sigmaX=0.5, sigmaY=0.5)
         return img
 
 
@@ -121,6 +131,5 @@ class PersonalizedBase(Dataset):
             interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
             img = cv2.resize(img, dsize=(self.size, self.size), interpolation=interp)
         return img
-
 
 
