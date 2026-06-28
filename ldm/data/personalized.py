a0be0704d167074035328476575d776b3e1fd0fa
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-06-28 13:21:49 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -67,8 +67,7 @@ class PersonalizedBase(Dataset):
 
     def noise(self, img):
         if random.random() < self.odds:
-            sig = random.randrange(5,11)
-            _noise = np.random.normal(0, sig, img.shape).astype(np.float32)
+            _noise = np.random.normal(0, 5, img.shape).astype(np.float32)
             img = img.astype(np.float32) + _noise
             img = np.clip(img, 0, 255).astype(np.uint8)
         return img
