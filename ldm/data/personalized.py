7460e07f345111cec8a4432b19d80d426671dbc8
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-04 13:50:47 -0400

    Fix indentation in noise method

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -59,7 +59,7 @@ class PersonalizedBase(Dataset):
 
     def noise(self, img):
         if random.random() < self.odds:
-           _noise = np.random.normal(0, 5, img.shape).astype(np.float32)
+            _noise = np.random.normal(0, 5, img.shape).astype(np.float32)
             img = img.astype(np.float32)
             noisy = cv2.add(img, _noise)
             img = np.clip(noisy, 0, 255).astype(np.uint8)
