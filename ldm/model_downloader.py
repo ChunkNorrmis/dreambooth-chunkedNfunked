e438bec3d478619cb67b11c9708944d5e748f17e
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-07 12:20:20 -0400

    Update model_downloader.py

--- ldm/model_downloader.py
+++ ldm/model_downloader.py
@@ -3,6 +3,7 @@ from huggingface_hub.file_download import hf_hub_download
 import hf_xet, hf_transfer
 
 
+
 def get_model(url):
     if url.startswith(('https://huggingface', 'https://www.huggingface')):
         return from_huggingface_hub(url)
@@ -25,10 +26,11 @@ def from_google_drive(url):
         if bytes_total is not None:
             print(f"\r{bytes_so_far / bytes_total * 100:.1f}%", end="")
 
-    ckpt_file = gdown.download(url=url, skip_download=True)
+    ckpt_file = gdown.download(url=url, skip_download=True)[-1]
     model_path = os.path.join(sys.path[0], ckpt_file)
     if not os.path.exists(model_path):
         print(f"Downloading '{ckpt_file}'...")
         gdown.download(url=url, output=model_path, quiet=True, progress=on_progress)
     return os.path.relpath(model_path)
+
 
