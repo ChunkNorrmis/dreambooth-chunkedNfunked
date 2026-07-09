ffbf24668368cc2d897d334580dcb85afa40e1fa
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-09 09:35:24 -0400

    Update model_downloader.py

--- ldm/model_downloader.py
+++ ldm/model_downloader.py
@@ -1,14 +1,30 @@
-import os, sys, gdown
+import os, sys, gdown, requests, re, urllib
 from huggingface_hub.file_download import hf_hub_download
 import hf_xet, hf_transfer
 
 
 
 def get_model(url):
+    if url.startswith('http://'):
+        url = url.replace('http://', 'https://')
     if url.startswith(('https://huggingface.co/', 'https://www.huggingface.co/')):
         return from_huggingface_hub(url)
     elif url.startswith(('https://drive.google.com/', 'https://www.drive.google.com/')):
         return from_google_drive(url)
+    else:
+        return from_generic(url)
+
+
+def from_generic(url):
+    response = requests.head(url, allow_redirects=True)
+    header = response.headers.get('content-disposition')
+    results = re.search(r'filename="?([^";]+)"?', header)
+    ckpt_file = match.group(1)
+    model_path = os.path.join(sys.path[0], ckpt_file)
+    if not os.path.exists(model_path):
+        print(f"Downloading '{ckpt_file}'...")
+        urllib.response.urlretrieve(url, model_path)
+    return os.path.relpath(model_path)
 
 
 def from_huggingface_hub(url):
