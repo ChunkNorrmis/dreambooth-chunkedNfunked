5567f231bc46136bb004dbddc5793125bd476e37
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-10 04:49:26 -0400

    Update copy_and_name_checkpoints.py

--- dreambooth_helpers/copy_and_name_checkpoints.py
+++ dreambooth_helpers/copy_and_name_checkpoints.py
@@ -13,35 +13,37 @@ def copy_and_name_checkpoints(config: JoePennaDreamboothConfigSchemaV1):
     log_ckpt_dir = config.log_checkpoint_directory()
     intermediate_checkpoints_directory = config.log_intermediate_checkpoints_directory()
     model_paths = [cp for cp in glob.glob(os.path.join(intermediate_checkpoints_directory, '*.ckpt')) + [os.path.join(log_ckpt_dir, 'last.ckpt')]]
+    n_models = len(model_paths)
     config.save_config_to_file(save_path=output_folder)
     if not os.path.exists(logs_directory):
         print(f"No checkpoints found in {logs_directory}")
         return
-    
-    if config.model_format == '.safetensors':
-        print(f"Depickling model checkpoint(s)")
-        print(' ')
-    p_bar = tqdm(total=len(model_paths), leave=False)
-    for model_path in model_paths:
+
+    if n_models >= 1:
         checkpoints_found = True
-        if os.path.basename(model_path) == 'last.ckpt':
-            if int(torch.load(model_path, map_location=torch.device('cpu'), weights_only=False)['global_step']) == config.max_training_steps:
-                last = os.path.join(output_folder, config.create_checkpoint_file_name(config.max_training_steps))
-                if config.model_format == '.safetensors':
-                    depicklize(model_path, nil_pickle=last)
-                else:
-                    shutil.move(model_path, last)
-                p_bar.update()
-        else:
-            file_name = os.path.basename(model_path)
-            steps = re.sub(r"epoch=\d{6}-step=0*", "", file_name)
-            steps = os.path.splitext(steps)[0]
-            output_file = os.path.join(output_folder, config.create_checkpoint_file_name(steps))
-            if config.model_format == '.safetensors':
-                depicklize(model_path, nil_pickle=output_file)
+        if config.model_format == '.safetensors':
+            print(f"Depickling model checkpoint(s)")
+            print(' ')
+        p_bar = tqdm(total=n_models), leave=False)
+        for model_path in model_paths:
+            if os.path.basename(model_path) == 'last.ckpt':
+                if int(torch.load(model_path, map_location=torch.device('cpu'), weights_only=False)['global_step']) == config.max_training_steps:
+                    last = os.path.join(output_folder, config.create_checkpoint_file_name(config.max_training_steps))
+                    if config.model_format == '.safetensors':
+                        depicklize(model_path, nil_pickle=last)
+                    else:
+                        shutil.move(model_path, last)
+                    p_bar.update()
             else:
-                shutil.move(model_path, output_file)
-            p_bar.update()
+                file_name = os.path.basename(model_path)
+                steps = re.sub(r"epoch=\d{6}-step=0*", "", file_name)
+                steps = os.path.splitext(steps)[0]
+                output_file = os.path.join(output_folder, config.create_checkpoint_file_name(steps))
+                if config.model_format == '.safetensors':
+                    depicklize(model_path, nil_pickle=output_file)
+                else:
+                    shutil.move(model_path, output_file)
+                p_bar.update()
             
     if checkpoints_found:
         print(f"✅ Model(s) moved to '{output_folder}'")
