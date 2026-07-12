8e8ead826d142ad2326b938c0c9465fe72c35f6c
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-12 08:56:38 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -4,7 +4,7 @@ def prune_checkpoint(checkpoint, float32=False):
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
-        pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ['state_dict', 'optimizer_states']}
+        pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ['state_dict', 'optimizer_states', 'pytorch-lightning_version', 'loops', 'callbacks']}
         pruned_checkpoint['state_dict'] = {k: v.contiguous() if float32 else v.contiguous().half() for k, v in checkpoint['state_dict'].items()}
         pruned_checkpoint['precision'] = 'fp32' if float32 else 'fp16'
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
