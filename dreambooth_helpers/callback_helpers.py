c6ffb2b3d26092e8c8a84ad1f7e63ae03022e7de
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-13 12:57:39 -0400

    Update callback_helpers.py

--- dreambooth_helpers/callback_helpers.py
+++ dreambooth_helpers/callback_helpers.py
@@ -163,8 +163,8 @@ class CUDACallback(Callback):
         epoch_time = time.time() - self.start_time
 
         try:
-            max_memory = trainer.training_type_plugin.reduce(max_memory)
-            epoch_time = trainer.training_type_plugin.reduce(epoch_time)
+            max_memory = trainer.strategy.reduce(max_memory)
+            epoch_time = trainer.strategy.reduce(epoch_time)
 
             rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
             rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
