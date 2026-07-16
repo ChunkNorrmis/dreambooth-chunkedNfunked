02fa8fd73a2b2b8d61d2649a5291ef0137bb1570
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-16 04:33:56 -0400

    Update pruner.py

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -6,7 +6,7 @@ def prune_checkpoint(checkpoint, float32=False):
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ['state_dict', 'optimizer_states', 'pytorch-lightning_version']}
         pruned_checkpoint['state_dict'] = {k: v.contiguous() if float32 else v.contiguous().half() for k, v in checkpoint['state_dict'].items()}
-        pruned_checkpoint['precision'] = 'fp32' if float32 else 'fp16'
+        pruned_checkpoint['model_precision'] = 'fp32' if float32 else 'fp16'
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
