d88fca83cd1ee40b0e6c6017664b450b2cbe02ca
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-16 20:31:48 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -28,10 +28,8 @@ class PersonalizedBase(Dataset):
 
         if per_image_tokens:
             assert self.n_imgs < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
-
         if set == 'train':
             self._length = self.n_imgs * epochs
-
         if self.reg:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
 
@@ -44,8 +42,10 @@ class PersonalizedBase(Dataset):
         img = cv2.imread(img_path)
         img = self.crop_and_resize(img)
         img = self.mirror(img)
-        img = self.augment(img)
+        img = random.chance([self.saturation, self.brightness])(img)
+        img = random.chance([self.blur, self.noise])(img)
         image = self.convert(img)
+        
         if self.reg:
             caption = generic_captions_from_path(img_path, self.data_root, self.reg_tokens)
         else:
@@ -70,50 +70,40 @@ class PersonalizedBase(Dataset):
 
 
     def noise(self, img):
-        if isinstance(img, torch.Tensor):
-            img = self.from_tensor(img)
-        n_str = random.randrange(1, 6)
-        _noise = np.random.normal(0, n_str, img.shape).astype(np.float32)
-        image = img.astype(np.float32)
-        noisy = cv2.add(image, _noise).astype(np.float32)
-        img = np.clip(noisy, 0, 255).astype(np.uint8)
+        if random.random() < 0.5:
+            if isinstance(img, torch.Tensor):
+                img = self.from_tensor(img)
+            strength = random.randrange(1, 4)
+            _noise = np.random.normal(0, strength, img.shape).astype(np.float32)
+            image = img.astype(np.float32)
+            noisy = cv2.add(image, _noise)
+            img = np.clip(noisy, 0, 255).astype(np.uint8)
         return img
 
     def posterize(self, img):
         img = self.to_tensor(img)
-        r = random.randrange(2,9,2)
+        r = random.randrange(2,7,2)
         img = fun.posterize(img, bits=r)
         return img
 
-    def saturate(self, img):
-        img = self.to_tensor(img)
-        f = random.uniform(0.9, 1.25)
-        img = fun.adjust_saturation(img, saturation_factor=f)
+    def saturation(self, img):
+        if random.random() < 0.5:
+            img = self.to_tensor(img)
+            amount = random.uniform(0.8, 1.3)
+            img = fun.adjust_saturation(img, saturation_factor=amount)
         return img
 
-    def brighten(self, img):
-        img = self.to_tensor(img)
-        f = random.uniform(0.8, 1.4)
-        img = fun.adjust_brightness(img, brightness_factor=f)
+    def brightness(self, img):
+        if random.random() < 0.5:
+            img = self.to_tensor(img)
+            amount = random.uniform(0.8, 1.5)
+            img = fun.adjust_brightness(img, brightness_factor=amount)
         return img
 
     def contrast(self, img):
         img = self.to_tensor(img)
         f = random.uniform(0.7, 1.3)
         img = fun.adjust_contrast(img, contrast_factor=f)
-        return img
-    
-    def augment(self, img):
-        if random.random() < 0.5:
-            img = random.choice([
-                #fun.equalize,
-                #self.posterize,
-                self.saturate,
-                #self.contrast,
-                self.noise,
-                self.blur,
-                self.brighten
-            ])(img)
         return img
 
 
@@ -131,11 +121,12 @@ class PersonalizedBase(Dataset):
         return img
 
 
-    def blur(self, img):                                                                                                                                                                                                
-        if isinstance(img, torch.Tensor):
-            img = self.from_tensor(img)
-        sig = random.uniform(0.6, 1.0)
-        img = cv2.GaussianBlur(img, (5, 5), sig)
+    def blur(self, img):
+        if random.random() < 0.5:
+            if isinstance(img, torch.Tensor):
+                img = self.from_tensor(img)
+            sig = random.uniform(0.45, 0.7)
+            img = cv2.GaussianBlur(img, (3, 3), sig)
         return img
 
 
