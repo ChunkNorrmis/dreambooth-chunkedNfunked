16eb4d3bc71d766740df5a210dadebeba495b6ac
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-20 21:41:24 -0400

    Enhance prune_checkpoint with token_classes parameter
    Added token_classes parameter to prune_checkpoint function.

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -1,12 +1,13 @@
 import os, sys, torch
 
-def prune_checkpoint(checkpoint, float32=False):
+def prune_checkpoint(checkpoint, float32=False, token_classes=None):
     if int(checkpoint['global_step']) > 0:
         print(f"This is global step {checkpoint['global_step']}.")
         print('Removing optimizer states from checkpoint')
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ['state_dict', 'optimizer_states', 'pytorch-lightning_version']}
         pruned_checkpoint['state_dict'] = {k: v.contiguous() if float32 else v.contiguous().half() for k, v in checkpoint['state_dict'].items()}
         pruned_checkpoint['model_precision'] = 'fp32' if float32 else 'fp16'
+        pruned_checkpoint['trained_tokens'] = token_classes
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
