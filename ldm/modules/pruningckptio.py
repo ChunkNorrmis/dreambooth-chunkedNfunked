3b324910369ddc785b63c94c7f88885d7bfa5c9e
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-20 21:38:45 -0400

    Update pruningckptio.py

--- ldm/modules/pruningckptio.py
+++ ldm/modules/pruningckptio.py
@@ -6,10 +6,11 @@ from pytorch_lightning.utilities.types import _PATH
 from ldm.pruner import prune_checkpoint
  
 class PruningCheckpointIO(TorchCheckpointIO):
-    def __init__(self, float32=False):
+    def __init__(self, float32=False, token_classes=None):
         self.float32 = float32
+        self.token_classes = token_classes
 
     def save_checkpoint(self, checkpoint, path, storage_options=None):
-        pruned_checkpoint = prune_checkpoint(checkpoint, float32=self.float32)
+        pruned_checkpoint = prune_checkpoint(checkpoint, float32=self.float32, token_classes=self.token_classes)
         TorchCheckpointIO.save_checkpoint(self, pruned_checkpoint, path, storage_options)
 
