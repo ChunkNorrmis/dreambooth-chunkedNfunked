38df474cb8f22dc9b88d03d16b145a6beb938a0e
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-22 17:20:12 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -108,7 +108,7 @@ class PersonalizedBase(Dataset):
         if random.random() < 0.25:
             if isinstance(img, np.ndarray):
                 img = self.to_tensor(img)
-            img = fun.adjust_contrast(img, contrast_factor=1.15)
+            img = fun.autocontrast(img)
         return img
 
 
