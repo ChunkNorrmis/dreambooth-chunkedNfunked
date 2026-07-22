757d9e12a5d9b7515a539021b9d8e136bb3275df
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-22 17:19:05 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -42,10 +42,9 @@ class PersonalizedBase(Dataset):
         img_path = self.imgs[i % self.n_imgs]
         img = cv2.imread(img_path)
         img = self.crop_and_resize(img)
-        if self.set == 'train' and not self.reg:
-            img = self.mirror(img)
-            img = self.noise(img)
-            img = self.blur(img)
+        img = self.mirror(img)
+        img = self.noise(img)
+        img = self.blur(img)
         image = self.convert(img)
         
         if self.reg:
