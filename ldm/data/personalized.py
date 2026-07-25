43c1b7176c2db5e67ba0bd793022998f29e5fe3f
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-25 11:06:00 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -13,7 +13,6 @@ class PersonalizedBase(Dataset):
                  size=512, epochs=100, center_crop=True, flip_p=0.5, mixing_prob=0.25, token_only=False, per_image_tokens=False):
 
         self.data_root = data_root
-        self.set = set
         self.imgs = [os.path.relpath(im) for im in glob.glob(os.path.join(self.data_root, '**', '*.png'), recursive=True)]
         self.n_imgs = len(self.imgs)
         self._length = self.n_imgs
@@ -29,7 +28,7 @@ class PersonalizedBase(Dataset):
 
         if per_image_tokens:
             assert self.n_imgs < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
-        if self.set == 'train':
+        if set == 'train':
             self._length = self.n_imgs * epochs
         if self.reg:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
@@ -39,37 +38,29 @@ class PersonalizedBase(Dataset):
         return self._length
 
     def __getitem__(self, i):
+        example = {}
         img_path = self.imgs[i % self.n_imgs]
         img = cv2.imread(img_path)
-        img = self.crop_and_resize(img)
-        img = self.mirror(img)
-        #img = self.noise(img)
-        #img = self.blur(img)
-        image = self.convert(img)
-        
-        if self.reg:
-            caption = generic_captions_from_path(img_path, self.data_root, self.reg_tokens)
-        else:
-            caption = caption_from_path(img_path, self.data_root, self.coarse_class_text, self.placeholder_token)
-        example = {'caption': caption, 'image': image}
-        return example
-        
-
-    def convert(self, img):
-        if isinstance(img, torch.Tensor):
-            img = img.permute(1, 2, 0)
-        if isinstance(img, np.ndarray):
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        image = np.array((img / 255. - 0.5) * 2).astype(np.float32)
-        return image
-
-
-    def mirror(self, img):
+        h, w = img.shape[:2]
+        crop = min(h, w)
+        if self.center_crop and h != w:
+            img = img[(h - crop) // 2: (h + crop) // 2, (w - crop) // 2: (w + crop) // 2]
+        if self.size != crop:
+            interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
+            img = cv2.resize(img, (self.size, self.size), interp)
         if random.random() < self.flip_p:
             img = cv2.flip(img, 1)
-        return img
-
-
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        example['image'] = np.array((img / 255. - 0.5) * 2).astype(np.float32)
+        
+        if self.reg:
+            example['caption'] = generic_captions_from_path(img_path, self.data_root, self.reg_tokens)
+        else:
+            example['caption'] = caption_from_path(img_path, self.data_root, self.coarse_class_text, self.placeholder_token)
+        
+        return example
+        
+    
     def noise(self, img):
         if random.random() < 0.25:
             if isinstance(img, torch.Tensor):
@@ -129,16 +120,5 @@ class PersonalizedBase(Dataset):
             if isinstance(img, torch.Tensor):
                 img = self.from_tensor(img)
             img = cv2.GaussianBlur(img, (3, 3), 0.55)
-        return img
-
-
-    def crop_and_resize(self, img):
-        h, w = img.shape[:2]
-        crop = min(h, w)
-        if self.center_crop and h != w:
-            img = img[(h - crop) // 2: (h + crop) // 2, (w - crop) // 2: (w + crop) // 2]
-        if self.size != crop:
-            interp = cv2.INTER_AREA if self.size < crop else cv2.INTER_CUBIC
-            img = cv2.resize(img, (self.size, self.size), interp)
         return img
 
