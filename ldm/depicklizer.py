bfab656d721739695c0c2afdc389cdad8f6e8cf7
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-26 13:16:02 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -5,18 +5,17 @@ import safetensors.torch as safetorch
 def depicklize(dict_pickle, nil_pickle=None):
     suspicious_pickle = torch.load(dict_pickle, map_location=torch.device('cpu'), weights_only=False)
     sus_dict = {k: v.contiguous() for k, v in suspicious_pickle['state_dict'].items()}
-    del suspicious_pickle['state_dict']
-    metadata = {k: f"{v}" for k, v in suspicious_pickle.items()}
-    metadata['format'] = 'pt'
-    if nil_pickle is None:
-        nil_pickle = dict_pickle.replace('.ckpt', '.safetensors')
-    elif os.path.isdir(nil_pickle):
-        nil_pickle = os.path.join(nil_pickle, os.path.basename(dict_pickle).replace('.ckpt', '.safetensors'))
-    elif not nil_pickle.endswith('.safetensors'):
-        nil_pickle = os.path.splitext(nil_pickle)[0] + '.safetensors'
     saved = safetorch.save(sus_dict)
     loaded = safetorch.load(saved)
     if equal_tensors(sus_dict, loaded):
+        if nil_pickle is None:
+            nil_pickle = dict_pickle.replace('.ckpt', '.safetensors')
+        elif os.path.isdir(nil_pickle):
+            nil_pickle = os.path.join(nil_pickle, os.path.basename(dict_pickle).replace('.ckpt', '.safetensors'))
+        elif not nil_pickle.endswith('.safetensors'):
+            nil_pickle = os.path.splitext(nil_pickle)[0] + '.safetensors'
+        metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
+        metadata['format'] = 'pt'
         safetorch.save_file(sus_dict, nil_pickle, metadata=metadata)
     else:
         print('!! Failed -- key mismatch')
