696e2816e443c877f7a24f1a94050e365ebcba18
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-07-28 12:29:49 -0400

    Replace trained_tokens with token_classes in checkpoint

--- ldm/pruner.py
+++ ldm/pruner.py
@@ -7,7 +7,7 @@ def prune_checkpoint(checkpoint, float32=False, token_classes=None):
         pruned_checkpoint = {k: v for k, v in checkpoint.items() if k not in ['state_dict', 'optimizer_states', 'pytorch-lightning_version']}
         pruned_checkpoint['state_dict'] = {k: v.contiguous() if float32 else v.contiguous().half() for k, v in checkpoint['state_dict'].items()}
         pruned_checkpoint['model_precision'] = 'fp32' if float32 else 'fp16'
-        pruned_checkpoint['trained_tokens'] = [f"{token_classes[0][0]}_{token_classes[0][1]}", f"{token_classes[1][0]}_{token_classes[1][1]}"]
+        pruned_checkpoint['trained_tokens'] = token_classes
         print(f"Checkpoint Keys: {pruned_checkpoint.keys()}")
         return pruned_checkpoint
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
