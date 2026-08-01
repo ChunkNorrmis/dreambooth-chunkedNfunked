2b1e94adc5f810f090760c9148b06659e7c551ed
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-08-01 12:53:58 -0400

    Update depicklizer.py

--- ldm/depicklizer.py
+++ ldm/depicklizer.py
@@ -8,14 +8,14 @@ def depicklize(dict_pickle, nil_pickle=None):
     saved = safetorch.save(sus_dict)
     loaded = safetorch.load(saved)
     if equal_tensors(sus_dict, loaded):
+        metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
+        metadata['format'] = 'pt'
         if nil_pickle is None:
             nil_pickle = dict_pickle.replace('.ckpt', '.safetensors')
         elif os.path.isdir(nil_pickle):
             nil_pickle = os.path.join(nil_pickle, os.path.basename(dict_pickle).replace('.ckpt', '.safetensors'))
         elif not nil_pickle.endswith('.safetensors'):
             nil_pickle = os.path.splitext(nil_pickle)[0] + '.safetensors'
-        metadata = {k: f"{v}" for k, v in suspicious_pickle.items() if k != 'state_dict'}
-        metadata['format'] = 'pt'
         safetorch.save_file(sus_dict, nil_pickle, metadata=metadata)
     else:
         print('!! Failed -- key mismatch')
